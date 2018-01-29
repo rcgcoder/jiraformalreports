31921c1c8d390fd16f7dc7d72c4fa42427eb5e54
RCGCoder <ricardo.cantabrana@gmail.com>
2018-01-29 13:00:21 +0100

    Zipped Launcher

---
+++ src/html/js/ZippedWebApp.js
@@ -0,0 +1,16 @@
+/*
+Class for download a Zip File with a lot of js files.
+.... extract files to persistent 
+.... load all files to memory
+*/
+
+class ZippedApp{
+	constructor(){
+		var self=this;
+		self.urlBase="";
+		log("ZippedApp Created");
+	}
+	run(appParams){
+		log("launched");
+	}
+}

25957dc430f8ba8dbd8a64eadec31e62b9cc564c
RCGCoder <ricardo.cantabrana@gmail.com>
2018-01-29 12:46:59 +0100

    change hello

--- src/html/js/UnZipApp.js
+++ src/html/js/UnZipApp.js
@@ -3,3 +3,17 @@ Class for download a Zip File with a lot of js files.
 .... extract files to persistent 
 .... load all files to memory
 */
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};

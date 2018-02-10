aabb5f6baa3dfc2d0fe837edd530c1e1a0178bb8
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-02-10 10:45:13 +0100

    error reading image?

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -5,14 +5,19 @@ class ZipWebApp{
 		self.twoParam="Coche";
 	}
 	run(){
+		console.log("starting ZipWebApp");
 		var self=this;
 		self.pushCallback(function(sPath,content){
+			console.log("Image Loaded:"+sPath);
 /*			var jqContainer=$("#"+self.getHtmlContainerID());
 			var jqImage=$("#jrfSplash");*/
 			var image=document.getElementById("jrfSplash");
 			image.src = content;
+			console.log("Image changed");
 		});
+		console.log("Requesting Image");
 		self.loadRemoteFile("img/reports2.jpg");
+		console.log("Runnin.... background");
 //		alert("Running ZipWebApp "); 
 	}
 }

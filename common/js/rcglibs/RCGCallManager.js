f6c9dc799b1e3b82077ebfc89d84e7033dc054e7
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-02-12 14:00:04 +0100

    new log

--- common/js/rcglibs/RCGCallManager.js
+++ common/js/rcglibs/RCGCallManager.js
@@ -428,6 +428,7 @@ class RCGCallManager{
 		var forkId=self.callManager.getRunningForkId();
 		var fncManagedCallback=function(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10){
 			self.callManager.setRunningForkId(forkId);
+			log("Calling Traditional Callback in fork:"+forkId);
 			fncTraditionalCallback(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10);
 		}
 		return fncManagedCallback;

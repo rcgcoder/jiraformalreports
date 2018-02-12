91daf05b57693c00492859b97f7dd19b33599f7a
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-02-12 16:22:34 +0100

    barrier works?

--- common/js/rcglibs/RCGZippedWebApp.js
+++ common/js/rcglibs/RCGZippedWebApp.js
@@ -595,7 +595,7 @@ class RCGZippedApp{
 	loadRemoteFileForks(arrRelativePaths){
 		var self=this;
 		var fncBarrierFinish=function(){
-			log("Barrier Finished");
+			log("Barrier Finished:" + self.callManager.getRunningForkId());
 			self.popCallback();
 		}
 		var barrier=new RCGBarrier(fncBarrierFinish,arrRelativePaths.length);

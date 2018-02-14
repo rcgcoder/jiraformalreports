7f9fbc376c7f1d47555bf70f04d74d0f984a14f1
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-02-14 06:53:41 +0100

    hack with lastCall

--- common/js/rcglibs/RCGCallManager.js
+++ common/js/rcglibs/RCGCallManager.js
@@ -234,6 +234,7 @@ class RCGCallManager{
 	}
 	getDeepStep(){
 		var self=this;
+		return self.getRunningLastCall();
 		if (self.steps.length==0) return self;
 		if (self.actStep>=self.steps.length) return self;
 		if (self.actStep<0) return self;

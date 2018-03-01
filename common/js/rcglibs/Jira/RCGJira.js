05f7c9aa331f0283c510a1d02a691b35ecbddde9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-01 08:16:53 +0100

    with xhr

--- common/js/rcglibs/Jira/RCGJira.js
+++ common/js/rcglibs/Jira/RCGJira.js
@@ -13,7 +13,25 @@ class RCGJira{
 	}
 	apiCall(sTargetUrl,sPage,sType,callback,arrHeaders){
 		var self=this;
-		// A simple POST request which logs response in the console.
+		var sUrl='/jira/rest/auth/1/session'; //sTargetUrl;
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', sUrl, true);
+		xhr.responseType = 'json';
+		request.setRequestHeader('Authorization', 'Basic ' + 'rcgcoder:jiraRitxar1676');
+		xhr.onerror=self.loadError;
+		xhr.onload = self.createManagedCallback(function(e) {
+		  if (xhr.status == 302) {
+			  var ghLink=xhr.getResponseHeader("Location");
+			  self.apiCall(ghLink);
+		  } else if (xhr.status == 200) {
+			  self.popCallback([xhr.response,xhr,sTargetUrl,arrHeaders]);
+		  } else {
+			  self.loadError({target:{src:sUrl}});			  
+		  }
+		});
+		xhr.send();	
+		
+/*		// A simple POST request which logs response in the console.
 		self.JiraAPConnection.request({
 		  url: '/jira/rest/auth/1/session',
 		  type: 'POST',
@@ -26,7 +44,7 @@ class RCGJira{
 		    console.log(arguments);
 		  }
 		});		
-	}
+*/	}
 	getAllIssues(){
 		var self=this;
 		self.pushCallback(function(response,xhr,sUrl,headers){

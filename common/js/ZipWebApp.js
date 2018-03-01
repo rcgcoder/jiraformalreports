4df2eb4ca01ef349c83c6de2508583fe5e15aff9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-01 06:56:44 +0100

    calling api with AP

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -35,9 +35,6 @@ class ZipWebApp{
 			var jira=new RCGJira(self);
 			jira.getAllIssues();
 		});
-		AP.getLocation(function(location){
-			  alert(location);
-			});
 		
 		self.continueTask();
 	}
--- common/js/rcglibs/Jira/RCGJira.js
+++ common/js/rcglibs/Jira/RCGJira.js
@@ -2,7 +2,7 @@ class RCGJira{
 	constructor(app){
 		var self=this;
 		self.repository="https://paega2.atlassian.net";
-		self.branch="";
+		self.JiraAPConnection=AP;
 		self.app=app;
 		self.headerAuth="";
 		self.ghStateString="_ungues";
@@ -13,32 +13,18 @@ class RCGJira{
 	}
 	apiCall(sTargetUrl,sPage,sType,callback,arrHeaders){
 		var self=this;
-		var sUrl=sTargetUrl;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', sUrl, true);
-		xhr.responseType = 'json';
-		if (typeof sType!=="undefined"){
-			xhr.responseType=sType;
-		}
-		if (typeof arrHeaders!=="undefined"){
-			for (var i=0;i<arrHeaders.length;i++){
-				xhr.setRequestHeader(arrHeaders[i].key, arrHeaders[i].value);
-			}
-		}
-		xhr.onerror=self.loadError;
-		xhr.onload = self.createManagedCallback(function(e) {
-		  var nRemaining=xhr.getResponseHeader("X-RateLimit-Remaining");
-		  log("Remaining GitHub Pets:"+nRemaining);
-		  if (xhr.status == 302) {
-			  var ghLink=xhr.getResponseHeader("Location");
-			  self.apiCall(ghLink);
-		  } else if (xhr.status == 200) {
-			  self.popCallback([xhr.response,xhr,sTargetUrl,arrHeaders]);
-		  } else {
-			  self.loadError({target:{src:sUrl}});			  
+		// A simple POST request which logs response in the console.
+		self.JiraAPConnection.request({
+		  url: '../assets/js/rest-example.json',
+		  type: 'POST',
+		  data: {name: 'some text', description: 'test'},
+		  success: function(responseText){
+		    console.log(responseText);
+		  },
+		  error: function(xhr, statusText, errorThrown){
+		    console.log(arguments);
 		  }
-		});
-		xhr.send();	
+		});		
 	}
 	getAllIssues(){
 		var self=this;

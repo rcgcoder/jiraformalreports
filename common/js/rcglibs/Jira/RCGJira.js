c4fcc96a4b003a5f8aab81f6072454a926e6c0b6
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-06 22:56:52 +0100

    getting all projects

--- common/js/rcglibs/Jira/RCGJira.js
+++ common/js/rcglibs/Jira/RCGJira.js
@@ -13,11 +13,29 @@ class RCGJira{
 	}
 	apiCall(sTargetUrl,sPage,sType,callback,arrHeaders){
 		var self=this;
-		
 		// A simple POST request which logs response in the console.
-		self.JiraAPConnection.request('/rest/api/2/issue/PDP-12/changelog')
+/*		self.JiraAPConnection.request('/rest/api/2/issue/PDP-12/changelog')
 		  .then(data => alert(data.body))
 		  .catch(e => alert(e.err));
+*/		
+		self.JiraAPConnection.request({
+			  url: sTargetUrl,
+			  type: 'POST',
+			  success: self.createManagedCallback(function(responseText){
+			    self.popCallback([responseText,xhr]);
+			  }),
+			  error: self.createManagedCallback(function(xhr, statusText, errorThrown){
+			    self.popCallback(["",xhr, statusText, errorThrown]);
+			  })
+			});
+	}
+	getAllProjects(){
+		var self=this;
+		self.pushCallback(function(response,xhr,sUrl,headers){
+			log("getAllProjects:"+response);
+			self.popCallback();
+		});
+		self.apiCall("/rest/api/2/project");
 	}
 	getAllIssues(){
 		var self=this;

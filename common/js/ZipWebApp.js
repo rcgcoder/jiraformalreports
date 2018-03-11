95764f84c9b94c8f506c635c09ce3b9b00d19741
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-11 12:52:39 +0100

    bugs

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -9,8 +9,8 @@ class ZipWebApp{
 		var self=this;
 		if (self.jira==""){
 			self.jira=new RCGJira(self);
-			jira.proxyPath=self.proxyPath;
-			jira.instance=self.urlBase;
+			self.jira.proxyPath=self.proxyPath;
+			self.jira.instance=self.urlBase;
 		}
 		return self.jira;
 	}

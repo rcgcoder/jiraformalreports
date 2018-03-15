9a668fbcf231e488c6c9ca279987c80a7dc4f2ce
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-15 08:23:01 +0100

    changing values

--- common/js/rcglibs/Jira/RCGJira.js
+++ common/js/rcglibs/Jira/RCGJira.js
@@ -136,7 +136,7 @@ class RCGJira{
 						self.popCallback();
 					});
 					self.apiCallApp(appInfo,sTarget,callType,data,nLast,1000,undefined,callback,arrHeaders);
-			},0,1,0,nBlockItems,undefined,"INNER",undefined
+			},0,nBlockItems,undefined,undefined,undefined,"INNER",undefined
 			);
 		};
 		var fncIteration=self.createManagedCallback(function(){

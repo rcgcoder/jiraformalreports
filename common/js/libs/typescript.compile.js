ba359248374d0d58fed894985352de494d821b77
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-16 08:07:53 +0100

    modify log

--- common/js/libs/typescript.compile.js
+++ common/js/libs/typescript.compile.js
@@ -54,7 +54,7 @@ class tsCompiler{
                 Close: function() {}
             };
         if(window.sessionStorage && sessionStorage.getItem('typescript' + hashcode)) {
-        	log("Loaded from session storage");
+        	log("Compiled result loaded from session storage");
             outfile.source = sessionStorage.getItem('typescript' + hashcode);
         } else {
             var compiler = new TypeScript.TypeScriptCompiler(outfile);

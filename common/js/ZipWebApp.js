15ddfdbe7881e3a54920877b26493131f0cf0214
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-16 20:53:50 +0100

    test to add angular tags after compile angular elements

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -21,6 +21,14 @@ class ZipWebApp{
 			log("Requesting Image");
 			self.loadRemoteFile("img/reports2.jpg");
 		});
+		
+		self.addStep("Loading angularjs and typescript inline compiler and Jira REST Client.... ",function(){
+			var arrFiles=[	//"ts/demo.ts",
+							"js/angular/angScript.ts",
+							"js/rcglibs/Jira/RCGJira.js"
+						 ]; //test
+			self.loadRemoteFiles(arrFiles);
+		});
 		self.addStep("changing image...",function(sPath,content){
 			log("Image Loaded:"+sPath);
 			var image=document.getElementById("jrfSplash");
@@ -29,14 +37,6 @@ class ZipWebApp{
 //			$('body').attr('ng-app', 'mySuperAwesomeApp');
 			$("#"+self.htmlContainerId).html("<heros></heros>");
 			self.popCallback(); // finishing the process.
-		});
-		
-		self.addStep("Loading angularjs and typescript inline compiler and Jira REST Client.... ",function(){
-			var arrFiles=[	//"ts/demo.ts",
-							"js/angular/angScript.ts",
-							"js/rcglibs/Jira/RCGJira.js"
-						 ]; //test
-			self.loadRemoteFiles(arrFiles);
 		});
 /*		self.addStep("Getting Confluence Oauth Token", function(){
 			var jira=self.getJira();

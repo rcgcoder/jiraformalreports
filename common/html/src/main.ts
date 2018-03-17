4e5f4e780daeec4f530b89ed929374e1fa27ad0b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-17 12:31:35 +0100

    enable prod mode

--- common/html/src/main.ts
+++ common/html/src/main.ts
@@ -1,4 +1,8 @@
 //main entry point
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app';
+import {enableProdMode} from "@angular/core";
+
+enableProdMode();
+
 platformBrowserDynamic().bootstrapModule(AppModule)

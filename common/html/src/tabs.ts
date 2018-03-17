c32e19a9839eb80af645e943f0191b551d7cf0b6
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-17 09:14:20 +0100

    testing

--- common/html/src/tabs.ts
+++ common/html/src/tabs.ts
@@ -5,7 +5,7 @@ import { Tab } from './tab';
   selector: 'tabs',
   template:`
     <ul class="nav nav-tabs">
-      <li *ngFor="let tab,tab2 of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
+      <li *ngFor="let tab of tabs" (click)="selectTab(tab)" [class.active]="tab.active">
         <a > {{tab.title}}</a>
       </li>
     </ul>

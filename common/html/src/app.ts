ded99a6f62e3ae021422524f7d73be10be3a0e8b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-17 08:56:35 +0100

    two tabs ts

--- common/html/src/app.ts
+++ common/html/src/app.ts
@@ -4,13 +4,14 @@ import {BrowserModule} from '@angular/platform-browser'
 
 import {Tabs} from './tabs';
 import {Tab} from './tab';
+import {Tab} from './tab2';
 
 @Component({
   selector: 'my-app',
   template: `
     <tabs>
       <tab [tabTitle]="'Tab 1'">Tab 1 Content</tab>
-      <tab tabTitle="Tab 2">Tab 2 Content</tab>
+      <tab2 tabTitle="Tab 2">Tab 2 Content</tab>
     </tabs>
   `
 })

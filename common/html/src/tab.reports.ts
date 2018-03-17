29b088a85ebcb68e494b807bf41bf884a963247e
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-17 11:18:10 +0100

    including tab.reports component

--- common/html/src/tab.reports.ts
+++ common/html/src/tab.reports.ts
@@ -1,9 +1,9 @@
 import { Component, Input, Output } from '@angular/core';
 @Component({
   selector: 'tabReports',
-  templateUrl: 'tab.reports.html',
+  templateUrl: './ngComponents/tab.reports.html',
 })
-export class CardComponent {
+export class TabReports {
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';
 }

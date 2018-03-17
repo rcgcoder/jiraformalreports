e6e7d2895608952df125f68b3ee69631ea8889e1
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-17 09:02:43 +0100

    closing tab2

--- common/html/src/app.ts
+++ common/html/src/app.ts
@@ -11,7 +11,7 @@ import {Tab2} from './tab2';
   template: `
     <tabs>
       <tab [tabTitle]="'Tab 1'">Tab 1 Content</tab>
-      <tab2 tabTitle="Tab 2">Tab 2 Content</tab>
+      <tab2 tabTitle="Tab 2">Tab 2 Content</tab2>
     </tabs>
   `
 })

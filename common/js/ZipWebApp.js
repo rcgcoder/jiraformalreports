52a5005edf42091c6645a4b369eaec503f16fb02
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-18 10:48:51 +0100

    testing systemjs and jira all together

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -48,56 +48,26 @@ class ZipWebApp{
 		      .catch(console.error.bind(console));
 		});
 
-/*		self.addStep("Loading angularjs and typescript files and Jira REST Client.... ",function(){
+		self.addStep("Loading Jira REST Client.... ",function(){
 			var arrFiles=[	//"ts/demo.ts",
-							"js/angular/angScript.ts",
 							"js/rcglibs/Jira/RCGJira.js"
 						 ]; //test
 			self.loadRemoteFiles(arrFiles);
 		});
 		
-		self.addStep("changing image...",function(sPath,content){
-			log("Image Loaded:"+sPath);
-			var image=document.getElementById("jrfSplash");
-			image.src = content;
-			log("Image changed");
-//			$('body').attr('ng-app', 'mySuperAwesomeApp');
-*/			
-/*		    angular
-		    	.module("mySuperAwesomeApp", [])
-		    	.component("heros", new HerosComponent());
-	*/	    /*
-		   	var html = '<div class="row dataPane"> Chunk of html elements </div>';
-			var trustedHtml = $sce.trustAsHtml(html);
-		    var compiledHtml = $compile(trustedHtml)($scope);
-		    angular.element(document).append(compiledHtml);
-		    */
-/*			$("#"+self.htmlContainerId).html(
-				'<div ng-controller="Tabs" class="panel panel-default" id="tabs-panel">'+ 
-				'  <tabset  class="panel-body">'+
-				'    <tab heading="Tab 1"> </tab>'+
-				'    <tab heading="Tab 2"> </tab>'+
-				'    <tab heading="Tab 3"> </tab>'+
-				'  </tabset>'+
-				'</div>'
-				);
-			angular.bootstrap(document, ["mySuperAwesomeApp"]);
-		
-			self.popCallback(); // finishing the process.
-		});
-*//*		self.addStep("Getting Confluence Oauth Token", function(){
+/*		self.addStep("Getting Confluence Oauth Token", function(){
 			var jira=self.getJira();
 			jira.proxyPath=self.proxyPath;
 			jira.instance=self.urlBase;
 			jira.oauthConfluenceConnect();
 		});
 */		
-/*		self.addStep("Getting All Issues.... ",function(){
+		self.addStep("Getting All Issues.... ",function(){
 			var jira=self.getJira();
 			jira.getAllIssues();
 		});
 */
-/*		self.addStep("Getting All Projects.... ",function(){
+		self.addStep("Getting All Projects.... ",function(){
 			var jira=self.getJira();
 			jira.getAllProjects();
 		});
@@ -105,7 +75,7 @@ class ZipWebApp{
 			var jira=self.getJira();
 			jira.getConfluence();
 		});
-*/		
+		
 		self.continueTask();
 	}
 

20b9fb4584619cdbbbd0154512c5049ce8d3d94d
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-20 09:29:19 +0100

    bug

--- common/js/rcglibs/RCGZippedWebApp.js
+++ common/js/rcglibs/RCGZippedWebApp.js
@@ -695,7 +695,7 @@ class RCGZippedApp{
 					var auxArrLibs=[];
 					var fncRequireSteps=self.createManagedCallback(function(sNameLib){
 				    	var sFileUrl=rcgUtilsManager.basePath+sNameLib;
-			    		self.pushCallback("Processing "+sFileUrl,function(){
+			    		self.pushCallback(function(){
 			    			var className=sNameLib.split(".")[0];
 			    			// Instantiate the object using the class name string
 			    			var auxObj = new className();

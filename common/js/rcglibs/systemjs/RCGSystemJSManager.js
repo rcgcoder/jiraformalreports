b9e8eab4e51e1c77529489157f8c6eb0d51ea1cb
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-24 20:32:26 +0100

    error to debug

--- common/js/rcglibs/systemjs/RCGSystemJSManager.js
+++ common/js/rcglibs/systemjs/RCGSystemJSManager.js
@@ -31,7 +31,8 @@ class RCGSystemJSManager{
 				var newUrl=self.app.composeUrl(sRelativePath);
 				return newUrl;
 			};
-			System.baseURL=System.composeUrl("");
+			adfasdfasd.substr(0,3);
+//			System.baseURL=System.composeUrl("");
 		    System.import('app')
 		      .catch(console.error.bind(console));
 		});

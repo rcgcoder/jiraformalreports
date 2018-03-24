cc686aee500879f5ca0fbac2621f045fd359ded0
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-24 20:15:48 +0100

    changing baseurl

--- common/systemjs/ts/main.ts
+++ common/systemjs/ts/main.ts
@@ -1,8 +1,8 @@
 //main entry point
+System.baseURL=System.composeUrl("";
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app';
 //import {enableProdMode} from "@angular/core";
 
 //enableProdMode();
-
 platformBrowserDynamic().bootstrapModule(AppModule);

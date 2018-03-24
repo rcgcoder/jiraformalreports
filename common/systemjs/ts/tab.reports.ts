e036ec17425ad43afdc8d6fad7903030cd741512
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-24 09:25:10 +0100

    new strategy

--- common/systemjs/ts/tab.reports.ts
+++ common/systemjs/ts/tab.reports.ts
@@ -3,6 +3,12 @@ import { Component, Input, Output } from '@angular/core';
   selector: 'tabReports',
   templateUrl: System.composeUrl('systemjs/html/tab.reports.html'),
 })
+function addNewItem(){
+    console.log("appending li to ul");
+    AJS.$("#ulDwarfers").append('<li><a href="#">Menu item Dyn</a></li>');
+}
+setTimeout(addNewItem,5000);
+
 export class TabReports {
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';

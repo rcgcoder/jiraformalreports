fb18e7266039533b38b567dd4c7f5984d62c09a9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-24 11:02:19 +0100

    removing rcgutils

--- common/js/rcglibs/RCGZippedWebApp.js
+++ common/js/rcglibs/RCGZippedWebApp.js
@@ -690,7 +690,7 @@ class RCGZippedApp{
 		var self=this;
 		if (typeof zip==="undefined"){
 			log("Zip engine is not running.... loading all utils");
-			self.pushCallback(function(){
+/*			self.pushCallback(function(){
 				var rcgUtilsManager=new RCGUtils();
 				rcgUtilsManager.requireLibs=self.createManagedCallback(function(bMakeGlobals,arrLibs){
 					var auxArrLibs=[];
@@ -717,7 +717,7 @@ class RCGZippedApp{
 				rcgUtilsManager.loadUtils(true);
 				self.popCallback();
 			});
-			var arrFiles=["css/RCGTaskManager.css",
+*/			var arrFiles=["css/RCGTaskManager.css",
 				          "js/libs/zip/zip.js"//,
 //						  "js/rcglibs/RCGBaseUtils.js",
 //						  "js/rcglibs/RCGUtils.js"

c77e84a90b07355f9fa37582bea270460982728b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-25 10:49:25 +0200

    new test

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -40,12 +40,12 @@ class ZipWebApp{
 						 ]; //test
 			self.loadRemoteFiles(arrFiles);
 		});
-/*		self.addStep("Launching Systemjs.... ",function(){
+		self.addStep("Launching Systemjs.... ",function(){
 			var sjs=self.getSystemjs();
 			sjs.loadEngine();
 		}//,0,1,undefined,undefined,undefined,"INNER",undefined
 		);
-*/
+
 		
 		
 /*		self.addStep("Getting All Issues.... ",function(){

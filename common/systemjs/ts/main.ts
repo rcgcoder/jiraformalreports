38ba52102aece001456961d6af5ab8675b5410b5
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-26 07:21:37 +0200

    in app

--- common/systemjs/ts/main.ts
+++ common/systemjs/ts/main.ts
@@ -1,6 +1,6 @@
 //main entry point
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app';
+import {AppModule} from System.composeUrl('app');
 //import {enableProdMode} from "@angular/core";
 
 //enableProdMode();

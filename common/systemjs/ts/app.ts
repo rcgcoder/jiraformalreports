20c992d43d54c6359d197071e911b700e6c853d4
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-26 07:10:04 +0200

    with composeurl

--- common/systemjs/ts/app.ts
+++ common/systemjs/ts/app.ts
@@ -2,7 +2,7 @@
 import {Component, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 
-import {Tabs} from './tabs';
+import {Tabs} from System.composeURL('systemjs/ts/tabs');
 import {Tab} from './tab';
 import {TabReports} from './tab.reports';
 import {TabConfig} from './tab.config';

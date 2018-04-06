04f3f178bf87385ac131b8a27fe1a24fc1d7dfe2
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-06 06:39:17 +0200

    adding the event handlers to postprocess function

--- common/systemjs/ts/tab.result.ts
+++ common/systemjs/ts/tab.result.ts
@@ -4,6 +4,20 @@ import { Component, Input, Output } from '@angular/core';
   templateUrl: System.composeUrl('systemjs/html/tab.result.html'),
 })
 export class TabResult {
+    constructor(){
+        System.addPostProcess(function(){
+             // Shows the dialog when the "Show dialog" button is clicked
+             AJS.$("#dialog-show-button").click(function(e) {
+                 e.preventDefault();
+                 AJS.dialog2("#demo-dialog").show();
+             });
+             // Hides the dialog
+             AJS.$("#dialog-submit-button").click(function (e) {
+                 e.preventDefault();
+                 AJS.dialog2("#demo-dialog").hide();
+             });
+        });
+ 
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';
 }

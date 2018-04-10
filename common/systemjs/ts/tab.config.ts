5352db1e328d3e24bcf08d16ee680b5df5239676
alpernia <alpernia@GO110117.itdeusto.local>
2018-04-10 17:40:20 +0200

    test combo

--- common/systemjs/ts/tab.config.ts
+++ common/systemjs/ts/tab.config.ts
@@ -4,7 +4,6 @@ import { Component, Input, Output } from '@angular/core';
   templateUrl: System.composeUrl('systemjs/html/tab.config.html'),
 })
 export class TabConfig {
-
     
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';

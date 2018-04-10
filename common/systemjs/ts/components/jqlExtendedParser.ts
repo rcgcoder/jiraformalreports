4e3a98fc4d4d5275b1c34ee405387cc5271bdc8f
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-10 07:05:06 +0200

    1 option maximun

--- common/systemjs/ts/components/jqlExtendedParser.ts
+++ common/systemjs/ts/components/jqlExtendedParser.ts
@@ -11,7 +11,7 @@ export class jqlExtendedParser {
     ngOnInit() {
         var self=this;
         System.addPostProcess(function(){
-            self.getSelect().auiSelect2();
+            self.getSelect().auiSelect2({maximumSelectionLength:1 });
             System.bindObj(self);
             var objSelector=self.getSelect();
             var arrOptions=[

3c7ecc168f562a923db459d6327ef816287614f9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-24 13:23:18 +0200

    another try

--- common/systemjs/ts/components/jqlSelector.ts
+++ common/systemjs/ts/components/jqlSelector.ts
@@ -1,10 +1,8 @@
 import { Component, Input, Output, OnInit } from '@angular/core';
+import {advSelector};
 @Component({
   selector: 'jqlSelector',
   templateUrl: System.composeUrl('systemjs/html/components/jqlSelector.html'),
 })
 export class jqlSelector extends advSelector {
-    constructor(){
-        super();
-    }
 }

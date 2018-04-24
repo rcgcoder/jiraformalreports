8e1c3b1e9ca5de0fa69decf95126a74291540e5b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-24 14:31:25 +0200

    evaluating jql string

--- common/systemjs/ts/components/jqlSelector.ts
+++ common/systemjs/ts/components/jqlSelector.ts
@@ -5,36 +5,26 @@ import {advSelector} from "./advSelector";
   templateUrl: System.composeUrl('systemjs/html/components/jqlSelector.html'),
 })
 export class jqlSelector extends advSelector {
+    prev_jql="";
     doOpenJQL(){
         var win = window.open("https://paega2.atlassian.net/issues/?jql=", '_blank');
         win.focus();
     }
+    getJQLBox(){
+        return AJS.$('[name="'+this.name+'-jqlEdit"]');
+    }
     
     onRetrieveTableData(theDlgSelector){
         log("Retrienving table data on jqlSelector");
-        super.onRetrieveTableData(theDlgSelector);
-/*        var theSelect=this.getSelect();
-        var nOps=theSelect[0].length; 
-        var arrTable=[];
-        for (var i=0;i<nOps;i++){
-            var opt=theSelect[0][i];
-            var key=opt.value;
-            var name=opt.text;
-            var isSelected=opt.selected;
-            var description=name;
-            var bLocated=false;
-            for (var j=0;(!bLocated)&&(j<this.elements.length);j++){
-                var elem=this.elements[j];
-                if (elem.key==key){
-                    bLocated=true;
-                    if (typeof elem.description!=="undefined"){
-                        description=elem.description;
-                    }
-                }
-            }
-            arrTable.push({key:key,name:description,selected:isSelected});
+        var theSelect=this.getSelect();
+        var theJQLBox=this.getJQLBox()[0];
+        var sJQL=theJQLBox.text();
+        if (this.prev_jql==sJQL){
+            log("Same jql:"+sJQL);
+        } else {
+            log("Diferent jql:"+sJQL);
+            this.prev_jql=sJQL;
         }
-        theDlgSelector.populateTable(arrTable);
-*/
+        super.onRetrieveTableData(theDlgSelector);
     }
 }

101eb635098e9a8658b629a8a57c87acf9102cca
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-24 13:53:19 +0200

    open tab from component

--- common/systemjs/ts/components/jqlSelector.ts
+++ common/systemjs/ts/components/jqlSelector.ts
@@ -5,4 +5,8 @@ import {advSelector} from "./advSelector";
   templateUrl: System.composeUrl('systemjs/html/components/jqlSelector.html'),
 })
 export class jqlSelector extends advSelector {
+    doOpenJQL(){
+        var win = window.open("https://paega2.atlassian.net/issues/?jql=", '_blank');
+        win.focus();
+    }
 }
--- common/systemjs/ts/tab.structure.ts
+++ common/systemjs/ts/tab.structure.ts
@@ -4,10 +4,6 @@ import { Component, Input, Output } from '@angular/core';
   templateUrl: System.composeUrl('systemjs/html/tab.structure.html'),
 })
 export class TabStructure {
-    doOpenJQL(){
-        var win = window.open("https://paega2.atlassian.net/issues/?jql=", '_blank');
-        win.focus();
-    }
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';
 }

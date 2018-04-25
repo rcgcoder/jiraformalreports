3107af79398c61b5a74b469f5a826c1152e67a6b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-25 09:58:50 +0200

    adding eventemitter

--- common/systemjs/ts/atlassianComponents/atlassianSelector.ts
+++ common/systemjs/ts/atlassianComponents/atlassianSelector.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import {advSelector} from "../components/advSelector";
 @Component({
   selector: 'atlassianSelector',

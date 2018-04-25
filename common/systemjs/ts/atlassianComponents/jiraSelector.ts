8f3680a00e647d2ccdbe08748dee67085f9c127f
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-25 07:43:45 +0200

    using jira and confluence

--- common/systemjs/ts/atlassianComponents/jiraSelector.ts
+++ common/systemjs/ts/atlassianComponents/jiraSelector.ts
@@ -5,17 +5,23 @@ import {advSelector} from "../components/advSelector";
   templateUrl: System.composeUrl('systemjs/html/atlassianComponents/jiraSelector.html'),
 })
 export class jiraSelector extends advSelector {
-    @Input() jiraProperty: string = 'fields';
-
-    getJira(){
-        return system.webapp.getJira();
+    @Input() atlassianObjectProperty: string = 'fields';
+    @Input() atlassianAplication: string = 'jira';
+    getPropertyValues(){
+        var obj;
+        if (this.atlassianAplication.toUpperCase()=="JIRA"){
+            obj=System.webapp.getJira();
+        } else if (this.atlassianAplication.toUpperCase()=="CONFLUENCE"){
+            obj=System.webapp.getConfluence();
+        }
+        return obj[this.atlassianObjectProperty];
     }
     
     onRetrieveTableData(theDlgSelector){
         var self=this;
         log("Retrieving table data on jiraSelector property:"+self.jiraProperty);
         var theSelect=self.getSelect();
-        var arrOptions=self.getJira()[self.jiraProperty];
+        var arrOptions=self.getPropertyValues();
         self.fillOptions(arrOptions);
         super.onRetrieveTableData(theDlgSelector);
     }

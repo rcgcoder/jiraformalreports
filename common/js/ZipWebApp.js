6105e21cc474339ae1fd8a18f48955237cfcf61d
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-26 10:01:28 +0200

    testing with a weight process at initialization

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -46,11 +46,11 @@ class ZipWebApp{
 		},0,1,undefined,undefined,undefined,"INNER",undefined
 		);
 		
-/*		self.addStep("Getting All Issues.... ",function(){
+		self.addStep("Getting All Issues.... ",function(){
 			var jira=self.getJira();
 			jira.getAllIssues();
 		});  
-*/
+
 		self.addStep("Getting All Project, issuetypes and field info.... ",function(){
 			var jira=self.getJira();
 			jira.getProjectsAndMetaInfo();

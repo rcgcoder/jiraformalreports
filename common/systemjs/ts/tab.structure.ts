07dcad2d5cd3d1687341eafa0cd5492b02fc3692
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-26 23:02:18 +0200

    getting values of relationship

--- common/systemjs/ts/tab.structure.ts
+++ common/systemjs/ts/tab.structure.ts
@@ -14,6 +14,42 @@ export class TabStructure {
         });
     }
  */
+    onGetBillingRelationships(event){
+        var arrValues=[
+                        "es implementado por"
+                        ,"causa"
+                        ,"está causada por"
+                        ,"depende de"
+                        ,"bloquea a"
+                        ,"detecta"
+                        ,"es detectado en"
+                        ,"duplica"
+                        ,"está duplicado por"
+                        ,"especifica"
+                        ,"es especificado en"
+                        ,"FF-depends on"
+                        ,"is FF-depended by"
+                        ,"FS-depends on"
+                        ,"is FS-depended by"
+                        ,"implementa"
+                        ,"tiene relación con"
+                        ,"est&aacute; relacionado con"
+                        ,"SF-depends on"
+                        ,"is SF-depended by"
+                        ,"SS-depends on"
+                        ,"is SS-depended by"
+                        ,"se divide en"
+                        ,"es subrequisito de"
+                        ,"valida"
+                        ,"es validado por"
+                        ];
+        var arrResult={};
+        for (var i=0;i<arrValues.length;i++){
+            var vAux=arrValues[i];
+            arrResult.push({key:vAux,name:vAux});
+        }
+        System.webapp.continueTask([arrResult]);
+    }
     onGetBillingFields(event){
        log("structure fields event.... onGetBillingFields");
        System.webapp.continueTask([System.webapp.getListFields()]);

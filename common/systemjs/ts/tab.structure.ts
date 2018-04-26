bccebe32e60185e2c06a9327b065447fca8b056c
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-26 22:35:26 +0200

    filling billing fields

--- common/systemjs/ts/tab.structure.ts
+++ common/systemjs/ts/tab.structure.ts
@@ -7,25 +7,14 @@ export class TabStructure {
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';
     @Input() name: string = 'tabStructure';
-/*    ngOnInit() {
+    ngOnInit() {
         var self=this;
         System.addPostProcess(function(){
             System.bindObj(self);
         });
     }
-*/    getBillingFieldSelect(){
-        var selAux=AJS.$('#selBillingField');
-        return selAux;
-    }
-    fillBillingFields(arrOps){
-        var selAux=this.getBillingFieldSelect();
-        for (var i=0;i<arrOps.length;i++){
-            var op=arrOps[i];
-            selAux.append('<option value="'+op.key+'">'+op.name+'</option>');
-        }
-    }
     onGetBillingFields(event){
-        log("structure fields event.... onGetBillingFields");
-        System.webapp.continueTask([[]]);
+       log("structure fields event.... onGetBillingFields");
+       System.webapp.continueTask([System.webapp.getListFields()]);
     }
 }

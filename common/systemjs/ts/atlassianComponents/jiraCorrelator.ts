b7ea16ce75b9c9f13a56fbdc4925b6f86438333e
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-28 06:24:01 +0200

    test filling txtArea

--- common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
+++ common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
@@ -17,6 +17,13 @@ export class jiraCorrelator {
     
     addField(){
         log("adding Field hierarchy");
+        var txtArea=System.getAngularDomObject(self.name+"-text");
+        txtArea=$(txtArea);
+        var sAntVal=txtArea.val();
+        if (sAntVal!=""){
+            sAntVal+="\n";
+        }
+        txtArea.val(sAntVal+" and " + " fieldX*333");
     }
     addLink(){
         log("adding Link hierarchy");

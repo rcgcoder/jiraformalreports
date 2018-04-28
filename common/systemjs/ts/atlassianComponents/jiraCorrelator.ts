8d5e92ba97495f132ed5f4adb2c0fd08cdbac382
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-28 06:06:40 +0200

    show hide "and" "or" at start and textarea changes

--- common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
+++ common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
@@ -6,14 +6,33 @@ import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 export class jiraCorrelator {
     @Input() name: string = 'jiraCorrelator';
     @Input() caption: string = 'Child Relations';
+    ngOnInit() {
+        var self=this;
+        System.addPostProcess(function(){
+            log("PostProcessing:"+self.name);
+            System.bindObj(self);
+            self.changeVisibilityAndOr();
+        });
+    }
+    
     addField(){
         log("adding Field hierarchy");
     }
     addLink(){
         log("adding Link hierarchy");
     }
-    textAreaChanged(event){
+    changeVisibilityAndOr(){
+        if ($("#"+self.name+"-text").value!=""){
+            $("#"+self.name+"-addOrField").show();
+            $("#"+self.name+"-addOrLink").show();
+        } else {
+            $("#"+self.name+"-addOrField").hide();
+            $("#"+self.name+"-addOrLink").hide();
+        }
         
+    }
+    textAreaChanged(event){
         log("TextArea Changed")
+        this.changeVisibilityAndOr();
     }
 }

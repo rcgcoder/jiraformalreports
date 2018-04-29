1b2fdfba016430557c8b000d314442f42792161e
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-04-29 06:51:35 +0200

    testing another way

--- common/js/rcglibs/RCGZippedWebApp.js
+++ common/js/rcglibs/RCGZippedWebApp.js
@@ -700,23 +700,22 @@ class RCGZippedApp{
 				var rcgUtilsManager=new RCGUtils();
 				rcgUtilsManager.requireLibs=self.createManagedCallback(function(bMakeGlobals,arrLibs){
 					var auxArrLibs=[];
-					self.addStep("Processing RCG Util:"+sNameLib,function(sNameLib){
-				    	var sFileUrl=rcgUtilsManager.basePath+sNameLib;
-			    		self.addStep("Loading "+sFileUrl,function(){
-				    		self.pushCallback(function(){
-				    			log("processing:"+sNameLib);
-				    			var className=sNameLib.split(".")[0];
-				    			// Instantiate the object using the class name string
-				    			var auxObj = new window[className](); 
-				    			rcgUtilsManager.makeGlobals(bMakeGlobals,auxObj);
-					    		self.popCallback();
-				    			});
-			    			self.loadRemoteFile(sFileUrl);
-			    			});
-					});
 			    	for (var i=0;i<rcgUtilsManager.arrLibs.length;i++){
 				    	var sNameLib=rcgUtilsManager.arrLibs[i];
-				    	fncRequireSteps(sNameLib);
+						self.addStep("Processing RCG Util:"+sNameLib,function(sNameLib){
+					    	var sFileUrl=rcgUtilsManager.basePath+sNameLib;
+				    		self.addStep("Loading "+sFileUrl,function(){
+					    		self.pushCallback(function(){
+					    			log("processing:"+sNameLib);
+					    			var className=sNameLib.split(".")[0];
+					    			// Instantiate the object using the class name string
+					    			var auxObj = new window[className](); 
+					    			rcgUtilsManager.makeGlobals(bMakeGlobals,auxObj);
+						    		self.popCallback();
+					    			});
+				    			self.loadRemoteFile(sFileUrl);
+				    			});
+						});
 				    }
 		    		self.continueTask();
 				});

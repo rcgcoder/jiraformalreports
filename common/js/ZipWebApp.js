0138ae788b4f4d7c4ffb48c8451e4017a11255db
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-01 14:58:07 +0200

    another way

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -252,7 +252,7 @@ class ZipWebApp{
 			$("#appMain").css('visibility','visible');
 			self.continueTask();
 		}));
-		self.initializationBarrier.add(self.getRunningTask());
+//		self.initializationBarrier.add(self.getRunningTask());
 		self.addStep("Initializing engines.... ",self.initialize);
 		self.addStep("Waiting to Barrier Reach.... ",function(){
 			log("Waiting to reach!.... not continues");

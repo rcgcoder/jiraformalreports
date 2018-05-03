c1506ed30f717f2d6504b2f8a2f4cbbf4dc42cd2
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-03 22:32:02 +0200

    new commit

--- common/systemjs/ts/components/listEditor.ts
+++ common/systemjs/ts/components/listEditor.ts
@@ -35,7 +35,7 @@ export class listEditor {
                                     </th>
                                 `);
             }
-        });
+        }); 
     }
     getTextArea(){
         var self=this;

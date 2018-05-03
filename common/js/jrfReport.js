53c5bb5a40987cc9d617a93782f48b2dcce79073
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-03 17:59:00 +0200

    loading root issues and projects

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -3,6 +3,9 @@ class jrfReport{
 		var self=this;
 		self.config=theConfig;
 		self.rootElements=[];
+		self.rootIssues=[];
+		self.rootProjects=[];
+		self.bFinishReport=false;
 		var tm=System.webapp.getTaskManager();
 		tm.extendObject(self);
 		self.jira=System.webapp.getJira();
@@ -23,10 +26,46 @@ class jrfReport{
 		// get root elements.... issues and/or projects
 		self.addStep("Getting root elements.... ",function(){
 			log("Getting root elements");
-			if (self.config.rootByJQL)
+			if (self.config.rootByJQL){
+				if (self.config.rootIssues.values.length>0){
+					self.rootIssues=self.config.rootIssues.values;
+				} else if (self.config.rootIssues.jql==""){
+					log("there is not root issues nor jql to do a report");
+					self.bFinishReport=true;
+				} else {
+					self.addStep("Executing jql:"+self.config.rootIssues.jql,function(){
+						self.jira.getJQLIssues(self.config.rootIssues.jql);
+					});
+					self.addStep("Setting as root issues all results of JQL:"+self.config.rootIssues.jql,function(arrIssues){
+						for (var i=0;i<arrIssues.length;i++){
+							var issue=arrIssues[i];
+							log("Issue:"+issue.id);
+							self.rootIssues.push(issue.id);
+						}
+					});
+				}
+			}
+			if (self.config.rootByProject){
+				if (self.config.rootProjects.length>0){
+					log("Loading projects selected to be roots");
+					self.rootProjects=self.config.rootProjects;
+				} else {
+					log("there is not projects selected to do a report");
+					self.bFinishReport=true;
+				}
+			}
 			self.continueTask();
 		});
-		
+		self.addStep("Processing root elements.... ",function(){
+			if (self.bFinishReport) return self.continueTask();
+			for (var i=0;i<self.rootIssues.length;i++){
+				log("Root Issue ["+i+"]: "+self.rootIssues[i]);
+			}
+			for (var i=0;i<self.rootProjects.length;i++){
+				log("Root Projects ["+i+"]: "+self.rootProjects[i]);
+			}
+			self.continueTask();
+		});
 		// assing childs and advance childs to root elements
 		// load report model and submodels
 		// replace the jrf Tokens

0a4b773398a877e290e1959753c073c1dfb5f172
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-04 07:02:55 +0200

    bug

--- common/systemjs/ts/dialogs/dlgEditableList.ts
+++ common/systemjs/ts/dialogs/dlgEditableList.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class dlgEditableList {
     @Input() name: string = 'dlgEditableList';
     @Input() columns: number = 1;
-    @Input() columnDefinitions: string={name:"key"}
+    @Input() columnDefinitions: string="[{caption:'key'}]";
     @Input() typeDescriptor: string = 'elements';
     @Input() openDialogCaption: string = '...';
     @Output() onApply = new EventEmitter<[]>();

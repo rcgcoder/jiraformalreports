36e61499f8bb4cfeefdd6101ba136baff3f2d756
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-09 12:40:17 +0200

    next step

--- common/js/modelProcessor/jrfForEach.js
+++ common/js/modelProcessor/jrfForEach.js
@@ -4,16 +4,17 @@ class jrfForEach{
 		self.tag=tag;
 		self.reportElem=reportElem;
 		self.model=model;
-		self.type=self.getAttribute("type");
-		self.subType=self.getAttribute("subtype");
-		self.where=self.getAttribute("where");
-		self.elemsInForEach;
+		self.type=tag.getAttribute("type");
+		self.subType=tag.getAttribute("subtype");
+		self.where=tag.getAttribute("where");
 		if (self.type="root"){
 			self.elemsInForEach=self.model.report.childs;
 		} else if (self.type="child"){
 			self.elemsInForEach=self.reportElem.getChilds();
 		} else if (self.type="advchild"){
 			self.elemsInForEach=self.reportElem.getAdvanceChilds();
+		} else {
+			self.elemsInForEach=newHashMap();
 		}
 	}
 	apply(){

febbbb963f2641c2daead8e535bb793529aed1b0
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-09 20:40:21 +0200

    its a while

--- common/js/modelProcessor/jrfModel.js
+++ common/js/modelProcessor/jrfModel.js
@@ -75,7 +75,7 @@ class jrfModel{
 		var sTagText=sHtml;
 		var indCloseTag=sTagText.indexOf(">");
 		var indOpenTag=sTagText.indexOf("<");
-		if ((indOpenTag>=0)&&(indOpenTag<indCloseTag)){// there is </p> into the jrf tag
+		while ((indOpenTag>=0)&&(indOpenTag<indCloseTag)){// there is </p> into the jrf tag
 			sTagText=sTagText.substring(0,indOpenTag)+sTagText.substring(indCloseTag+1,sTagText.length);
 			indCloseTag=sTagText.indexOf(">");
 			indOpenTag=sTagText.indexOf("<");

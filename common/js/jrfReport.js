5a9b717276095855caa07d78bd8ca4931be459e9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-10 19:08:22 +0200

    remove alert !!!! IT WORKS !!!!1

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -35,7 +35,6 @@ var jrfReport=class jrfReport {
 	}
 	execute(){
 		var self=this;
-		alert("Testing hot reload");		
 		self.addStep("Getting Confluence Report Model.... ",function(){
 			var cfc=System.webapp.getConfluence();
 			//cfc.getAllPages();

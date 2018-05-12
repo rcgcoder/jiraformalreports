031e08d189601d249263019911bb163e7bef60ca
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-12 09:55:06 +0200

    correct replacing

--- common/js/modelProcessor/jrfToken.js
+++ common/js/modelProcessor/jrfToken.js
@@ -109,11 +109,11 @@ var jrfToken=class jrfToken{ //this kind of definition allows to hot-reload
 					vVar=self.variables.getVars(varName);
 				}
 				
-				if (vVar.length()==0){
-					vVar.push(varValue);
-				} else {
-					vVar.top().value=varValue;
+				if (vVar.length()>0){// replace as a pop/push
+					vVar.pop();
 				}
+				vVar.push(varValue);
+
 				log("Value ["+varName+"] setted:["+varValue+"]");
 			}
 		}

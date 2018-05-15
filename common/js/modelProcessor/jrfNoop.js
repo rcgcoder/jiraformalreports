7936b680abfd18de26c2e4f34122ad192765a53c
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-15 07:36:59 +0200

    changing noop

--- common/js/modelProcessor/jrfNoop.js
+++ common/js/modelProcessor/jrfNoop.js
@@ -6,12 +6,13 @@ var jrfNoop=class jrfNoop{//this kind of definition allows to hot-reload
 	}
 	apply(){
 		var self=this;
+		var noopIndHtmlBuffer=self.pushHtmlBuffer();
 		self.addStep("Processing all Childs of NOOP",function(){
 			self.processAllChilds();
 		});
 		self.addStep("Finalizing the Noop process",function(){
 			self.addPostHtml();
-			var sContent=self.popHtmlBuffer(self.indTokenHtmlBuffer);
+			var sContent=self.popHtmlBuffer(noopIndHtmlBuffer);
 			sContent=self.replaceVars(sContent);
 			self.addHtml(sContent);
 			self.continueTask();

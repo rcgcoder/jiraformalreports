ea324e4ce5984abdcedd400f0f742c84006e3191
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-15 06:25:24 +0200

    bug in noop

--- common/js/modelProcessor/jrfNoop.js
+++ common/js/modelProcessor/jrfNoop.js
@@ -2,20 +2,19 @@ var jrfNoop=class jrfNoop{//this kind of definition allows to hot-reload
 	constructor(tag,reportElem,model){
 		var self=this;
 		model.extendToken(self,tag,reportElem);
-//		self.autoAddPostHtml=false;
+		self.autoAddPostHtml=false;
 	}
 	apply(){
 		var self=this;
-		self.pushHtmlBuffer();
 		self.addStep("Processing all Childs of NOOP",function(){
 			self.processAllChilds();
 		});
 		self.addStep("Finalizing the Noop process",function(){
 			self.addPostHtml();
-/*			var sContent=self.popHtmlBuffer();
+			var sContent=self.popHtmlBuffer(self.indTokenHtmlBuffer);
 			sContent=self.replaceVars(sContent);
 			self.addHtml(sContent);
-*/			self.continueTask();
+			self.continueTask();
 		});
 	}
 

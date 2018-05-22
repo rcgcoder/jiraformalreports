28afae811b8931c4f67d2a2d2e91cac2fa5aa86c
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-22 15:39:39 +0200

    correct sjql

--- common/js/rcglibs/atlassian/RCGJira.js
+++ common/js/rcglibs/atlassian/RCGJira.js
@@ -253,7 +253,7 @@ class RCGJira{
 		});
 		sJQL="issue in ("+sJQL+")";
 		self.addStep("Getting All Issues from JQL", function(){
-			self.getFullList("/rest/api/2/search?fields=comment&expand=renderedFields&jql="+jql,"issues",undefined,undefined,cbBlock);
+			self.getFullList("/rest/api/2/search?fields=comment&expand=renderedFields&sJQL="+jql,"issues",undefined,undefined,cbBlock);
 		});
 		self.addStep("Returning all Issues from JQL", function(response,xhr,sUrl,headers){
 			self.continueTask([response]);

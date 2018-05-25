11c344fc2fb6ce2895a67eed0abbe68af33971dc
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-25 06:29:41 +0200

    starting docx engine

--- common/systemjs/ts/components/docxSaver.ts
+++ common/systemjs/ts/components/docxSaver.ts
@@ -10,9 +10,24 @@ export class docxSaver {
         System.addPostProcess(function(){
             log("PostProcessing:"+self.name);
             System.bindObj(self);
+            System.webapp.getTaskManager().extendObject(self);
         });
     }
     saveDocx(){
+        var self=this;
         log("Saving to file");
+        // download the docxtemplater.js
+        self.addStep("Loading docx templater engine.... ",function(){
+            var arrFiles=[  //"ts/demo.ts",
+                            "js/libs/docxtemplater.v3.6.3.js"
+                            ]; //test
+            System.webapp.loadRemoteFiles(arrFiles);
+        });
+        
+        // download the docx template
+        self.addStep("Downloading Template",function(){
+           System.webapp.loadRemoteFile("docx/html.docx"); 
+        });
+        // apply
     }
 }

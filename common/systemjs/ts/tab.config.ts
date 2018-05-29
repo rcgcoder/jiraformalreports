e6782e21bb29c0b71acf872f1467b40a5881d1f6
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-05-29 05:49:34 +0200

    realocate methods

--- common/systemjs/ts/tab.config.ts
+++ common/systemjs/ts/tab.config.ts
@@ -7,4 +7,27 @@ export class TabConfig {
     
     @Input() header: string = 'this is header';   
     @Input() footer: string = 'this is footer';
+    onChangeInterest(event){
+        this.updateCorrelators();
+    }
+    updateCorrelators(){
+        var arrFields=[];
+        
+        var auxObj=System.getAngularObject('selInterestFields',true);
+        var arrValues=auxObj.getSelectedValues();
+        arrFields=arrFields.concat(arrValues);
+        
+        auxObj=System.getAngularObject('selInterestOtherFields',true);
+        arrValues=auxObj.getSelectedValues();
+        arrFields=arrFields.concat(arrValues);
+
+        auxObj=System.getAngularObject('selInterestIssueLinkTypes',true);
+        var arrLinks=auxObj.getSelectedValues();
+        
+        System.getAngularObject('BillingHierarchy',true).fillFields(arrFields);
+        System.getAngularObject('AdvanceHierarchy',true).fillFields(arrFields);
+        System.getAngularObject('BillingHierarchy',true).fillLinks(arrLinks);
+        System.getAngularObject('AdvanceHierarchy',true).fillLinks(arrLinks);
+    }
+
 }
--- common/systemjs/ts/tab.structure.ts
+++ common/systemjs/ts/tab.structure.ts
@@ -231,28 +231,6 @@ export class TabStructure {
         self.updateCorrelators();
         
     }
-    onChangeInterest(event){
-        this.updateCorrelators();
-    }
-    updateCorrelators(){
-        var arrFields=[];
-        
-        var auxObj=System.getAngularObject('selInterestFields',true);
-        var arrValues=auxObj.getSelectedValues();
-        arrFields=arrFields.concat(arrValues);
-        
-        auxObj=System.getAngularObject('selInterestOtherFields',true);
-        arrValues=auxObj.getSelectedValues();
-        arrFields=arrFields.concat(arrValues);
-
-        auxObj=System.getAngularObject('selInterestIssueLinkTypes',true);
-        var arrLinks=auxObj.getSelectedValues();
-        
-        System.getAngularObject('BillingHierarchy',true).fillFields(arrFields);
-        System.getAngularObject('AdvanceHierarchy',true).fillFields(arrFields);
-        System.getAngularObject('BillingHierarchy',true).fillLinks(arrLinks);
-        System.getAngularObject('AdvanceHierarchy',true).fillLinks(arrLinks);
-    }
     loadDefaultReport(){
         var self=this;
         var fileName="defaultReportConfig.json";

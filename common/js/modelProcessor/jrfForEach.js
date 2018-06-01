177c436b1da54cf84db7142658053a629259dbc7
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-06-01 06:17:37 +0200

    bug

--- common/js/modelProcessor/jrfForEach.js
+++ common/js/modelProcessor/jrfForEach.js
@@ -143,11 +143,11 @@ var jrfForEach=class jrfForEach{//this kind of definition allows to hot-reload
 				processedItemNumber++;
 				if ((self.subType=="row")
 						&&(bLastShowed)
-						&&((processedItemNumber+processedItemJumped)<(self.elemsInForEach.length()))){
+						&&((processedItemNumber+processedItemJumped)<(elemsInForEach.length()))){
 					self.addHtml("<!-- ADDED BY FOREACH ROW ==>>  --></td></tr><tr><td><!-- <== ADDED BY FOREACH ROW -->");
 				} else if ((self.subType=="subrow")
 							&&(bLastShowed)
-							&&((processedItemNumber+processedItemJumped)<(self.elemsInForEach.length()))){
+							&&((processedItemNumber+processedItemJumped)<(elemsInForEach.length()))){
 					self.addHtml("<!-- ADDED BY FOREACH SUBROW ==>>  --></td></tr><tr><td><!-- <== ADDED BY FOREACH SUBROW -->");
 				}
 				self.continueTask();

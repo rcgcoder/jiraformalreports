3fee73d86ffa32617c0998e6ef3e5fc1f2cc3bd9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-06-13 05:01:59 +0200

    billing phase system jrfplugin

--- common/jrfPlugins/js/addFaseInfo.js
+++ common/jrfPlugins/js/addFaseInfo.js
@@ -72,6 +72,7 @@ var issueExtender=class issueExtender{//this kind of definition allows to hot-re
 		tEstados["Resuelta"]=3;
 		tEstados["Resuelto"]=3;
 		tEstados["Reabierta"]=0;
+		tEstados[""]=-1;
 		var iResult=tEstados[sValue];
 		if (typeof iResult!=="undefined"){
 			return iResult;
@@ -80,34 +81,191 @@ var issueExtender=class issueExtender{//this kind of definition allows to hot-re
     }
 
     getFaseName(nFaseNum){
-			if (nFaseNum==5){
-				return "Total";
-			} else if (nFaseNum==4){
+    		if (nFaseNum==4){
 				return "Desplegado";
 			} else if (nFaseNum==3){
 				return "Implementado";
 			} else if (nFaseNum==2){
-				return "Disenado";
+				return "Diseñado";
 			} else if (nFaseNum==1){
 				return "Aprobado";
-			} else { //if (FaseNum==0)
+			} else if (FaseNum==0) {
 				return "Identificado";
+			} else {
+				return "No creado";
 			}
      }
-    /*
-	obj.importesPorcentajeRef={"Total":1,"Identificado":0,"Aprobado":0,"Disenado":0.1,"Implementado":0.8,"Desplegado":0.1};
-	obj.importesEstimados={"Total":impAux,"Aprobado":0,"Disenado":impAux*0.1,
-							"Implementado":impAux*0.8,"Desplegado":impAux*0.1,
-							"Identificado":0
-							};
-	obj.importesReales={"Total":impAux*rAux,"Aprobado":0,"Disenado":impAux*0.1*(1+rAux),
-									"Implementado":impAux*0.8*(1+rAux),
-									"Desplegado":impAux*0.1*(1*rAux),
-									"Identificado":0
-							};
-	obj.importesdefinidos={importesEstimados:{"Total":false,"Identificado":false,"Aprobado":false,"Disenado":false,"Implementado":false,"Desplegado":false},
-						  importesReales:{"Total":false,"Identificado":false,"Aprobado":false,"Disenado":false,"Implementado":false,"Desplegado":false}};
-    */
+    getFieldFaseBillingName(nFaseNum){
+		if (nFaseNum==5){
+			return "Total";
+		} else if (nFaseNum==4){
+			return "Desplegado";
+		} else if (nFaseNum==3){
+			return "Implementado";
+		} else if (nFaseNum==2){
+			return "Disenado";
+		} else if (nFaseNum==1){
+			return "Aprobado";
+		} else if (FaseNum==0) {
+			return "Identificado";
+		} else {
+			return "NoCreado";
+		}
+    }
+    initilizeBilling(atDatetime){
+    	var objImportes={};
+		var faseActual=this.fieldValue("Fase",false,atDatetime);
+		var timeoriginalestimate=this.fieldValue("timeoriginalestimate",false,atDatetime);
+		var timeestimate=this.fieldValue("timeestimate",false,atDatetime);
+		var timespent=this.fieldValue("timespent",false,atDatetime);
+		if (timeoriginalestimate=="") timeoriginalestimate=0; else timeoriginalestimate=parseFloat(timeoriginalestimate);
+		if (timeestimate=="") timeestimate=0; else timeestimate=parseFloat(timeestimate);
+		if (timespent=="") timespent=0; else timespent=parseFloat(timespent);
+    	objImportes.source={
+    				  timeoriginalestimate:timeoriginalestimate,
+    				  timeestimate:timeestimate,
+    				  timespent:timespent,
+    				  faseActual:faseActual,
+	    			  importesEstimados:{"Total":"","Identificado":"","Aprobado":"","Disenado":"","Implementado":"","Desplegado":""},
+					  importesReales:{"Total":"","Identificado":"","Aprobado":"","Disenado":"","Implementado":"","Desplegado":""},
+					  bImportesRealesBlanco:false,
+					  bImportesRealesCero:false,
+					  acumFasesEstimado:0,
+					  acumFasesReal:0
+    				};
+    	objImportes.importesPorcentajeRef={"Total":1,"Identificado":0,"Aprobado":0,"Disenado":0.1,"Implementado":0.8,"Desplegado":0.1};
+    	objImportes.importesEstimados={"Total":0,"Aprobado":0,"Disenado":0,
+    							"Implementado":0,"Desplegado":0,
+    							"Identificado":0
+    							};
+    	objImportes.importesReales={"Total":0,"Aprobado":0,"Disenado":0,
+    									"Implementado":0,
+    									"Desplegado":0,
+    									"Identificado":0
+    							};
+    	objImportes.importesdefinidos={importesEstimados:{"Total":false,"Identificado":false,"Aprobado":false,"Disenado":false,"Implementado":false,"Desplegado":false},
+    						  importesReales:{"Total":false,"Identificado":false,"Aprobado":false,"Disenado":false,"Implementado":false,"Desplegado":false}};
+   
+    	var arrFields=[];
+    	arrFields.push(["Importe Estimado Diseño","Disenado","importesEstimados"]);
+    	arrFields.push(["Importe Estimado Implementación","Implementado","importesEstimados"]);
+    	arrFields.push(["Importe Estimado Despliegue","Desplegado","importesEstimados"]);
+    	arrFields.push(["Importe Estimado Total","Total","importesEstimados"]);
+
+    	arrFields.push(["Importe Real Aprobación","Aprobado","importesReales"]);
+    	arrFields.push(["Importe Real Diseño","Diseñado","importesReales"]);
+    	arrFields.push(["Importe Real Implementación","Implementado","importesReales"]);
+    	arrFields.push(["Importe Real Despliegue","Desplegado","importesReales"]);
+    	arrFields.push(["Importe Real Total","Total","importesReales"]);
+    	var vValue;
+    	var bWithValue;
+    	arrFields.forEach(function (fieldImporte){
+    		bWithValue=false;
+    		vValue=this.fieldValue(fieldImporte[0],false,atDatetime);
+    		if ((vValue=="") || (isUndefined(vValue)))
+    			vValue=0;
+    		else {
+    			vValue=parseFloat(vValue);
+    			bWithValue=true;
+    		}
+			objImportes.importesdefinidos[fieldImporte[2]][fieldImporte[1]]=bWithValue;
+			objImportes.source[fieldImporte[2]][fieldImporte[1]]=vValue;
+    	});
+    	
+		var bUndefinedEstimado=false;
+		var bUndefinedReal=false;
+		var impEst=0;
+		var impReal=0;
+		var fieldFaseName;
+		for (var nFase=0;nFase<6;nFase++){
+			bUndefinedEstimado=false;
+			bUndefinedReal=false;
+			impEst=0;
+			impReal=0;
+			fieldFaseName=this.getFieldFaseBillingName(nFase);
+			bUndefinedEstimado=(!objImportes.importesdefinidos.importesEstimados[fieldFaseName]);
+			bUndefinedReal=(!objImportes.importesdefinidos.importesReales[fieldFaseName]);
+			if (!bUndefinedEstimado) impEst=objImportes.source.importesEstimados[fieldFaseName];
+			if (!bUndefinedReal) impReal=objImportes.source.importesReales[fieldFaseName];
+			if (bUndefinedEstimado) impEst=impReal;
+			if (bUndefinedReal) impReal=impEst;
+			objImportes.importesEstimados[fieldFaseName]=impEst;
+			objImportes.importesReales[fieldFaseName]=impReal;
+			if (nFase<5) {
+				objImportes.acumFasesEstimado+=impEst;
+				objImportes.acumFasesReal+=impReal;
+			}
+		}
+    	return objImportes;
+    }
+    
+    getBilling(atDatetime,hourCost){
+    	// initialize and load the importes structure
+    	var objImportes=initilizeImportes(atDatetime);
+    	var vPorc;
+		var tEstimado;
+		var tReal;
+		var totalEstimado;
+		var totalReal;
+		
+		tReal=objImportes.source.timespent;
+		tEstimado=objImportes.source.timeestimate;
+		
+		var servicio=this.fieldValue("project.key");
+		var tipo=this.fieldValue("issuetype.name");
+		
+		if (servicio!="OT"){
+			if ((tipo=="Tarea")||(tReal==0)) tReal=tEstimado;
+			if (tEstimado==0) tEstimado=tReal;
+		}
+		
+		var acumFasesEstimado=objImportes.acumFasesEstimado;
+		var acumFasesReal=objImportes.acumFasesReal;
+		var fieldFaseName;
+		if ((acumFasesEstimado.toFixed(1)==1)||(acumFasesEstimado.toFixed(1)==0)){
+			if ((objImportes.importesEstimados.Total.toFixed(1)==1)||(objImportes.importesEstimados.Total.toFixed(1)==0)){
+				// si el total estimado es 0 o es 1 quiere decir que hay que cogerlo del tiempo de jira.
+				totalEstimado=(tEstimado/(60*60))*hourCost;
+			} else {
+				// si hay un total estimado.... se coge del total..
+				totalEstimado=objImportes.importesEstimados.Total;
+			}
+			objImportes.importesEstimados.Total=totalEstimado;
+			if (acumFasesEstimado.toFixed(1)==0) { //si la suma de las partes es 0 quiere decir que hay que ponerlo todo en alguna fase.. o en ninguna si son issues creados despues del 22 de febrero.
+				if (tipo=="Despliegue") {	// si es un despliegue se pone en la fase 4
+					objImportes.importesEstimados[this.getFieldFaseBillingName(4)]=1;
+				}
+			}
+			for (var nFase=1;nFase<5;nFase++){
+				fieldFaseName=this.getFieldFaseBillingName(nFase);
+				vPorc=objImportes.importesEstimados[fieldFaseName];
+				objImportes.importesEstimados[fieldFaseName]=totalEstimado*vPorc;
+			}
+		}
+		
+		if ((acumFasesReal.toFixed(1)==1)||(acumFasesReal.toFixed(1)==0)){
+			if ((objImportes.importesReales.Total.toFixed(1)==1)
+				||(objImportes.importesReales.Total.toFixed(1)==0)){
+				totalReal=(tReal/(60*60))*hourCost;
+			} else {
+				totalReal=objImportes.importesReales.Total;
+			}
+			objImportes.importesReales.Total=totalReal;
+			if (acumFasesReal.toFixed(1)==0) {
+				if (tipo=="Despliegue") {	// si es un despliegue se pone en la fase 4
+					objImportes.importesReales[fncGetFieldFase(4)]=1;
+				}
+			}
+			for (var nFase=1;nFase<5;nFase++){
+				fieldFaseName=this.getFieldFaseBillingName(nFase);
+				vPorc=objImportes.importesReales[fieldFaseName];
+				objImportes.importesReales[fieldFaseName]=totalReal*vPorc;
+			}
+		}
+		return objImportes;
+    }
+    
+    
     execute(){
          var self=this;
          var fncGetFase=function(){
@@ -143,6 +301,9 @@ var issueExtender=class issueExtender{//this kind of definition allows to hot-re
                  issue.getFaseLife=fncGetFaseLife;
                  issue.getFaseName=fncGetFaseName;
                  issue.getFaseNameLife=fncGetFaseNameLife;
+                 issue.getFieldFaseBillingName=this.getFieldFaseBillingName;
+                 issue.initilizeBilling=self.initilizeBilling;
+                 issue.getBilling=self.getBilling;
          });
     }
 

b8fd772d46bde2fddfa86df158f78d6b777eb6ce
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-06-17 07:43:52 +0200

    return with blocks of calls

--- common/js/rcglibs/RCGTaskManager.js
+++ common/js/rcglibs/RCGTaskManager.js
@@ -451,7 +451,7 @@ class RCGTaskManager{
 		//self.extendObject(obj);
 		self.asyncTaskCalls=true;
 		self.asyncTaskCallsDelay=0;
-		self.asyncTaskCallsBlock=0;
+		self.asyncTaskCallsBlock=3000;
 		self.timeoutsCalled=0;
 		self.timeoutsAvoided=0;
 		self.lastTimeout=0;

6ec6b03400ff45b8daee5bc4ace1c8fae86ff843
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-06-17 09:54:29 +0200

    returned without blocks

--- common/js/rcglibs/RCGTaskManager.js
+++ common/js/rcglibs/RCGTaskManager.js
@@ -452,7 +452,7 @@ class RCGTaskManager{
 		//self.extendObject(obj);
 		self.asyncTaskCalls=true;
 		self.asyncTaskCallsDelay=0;
-		self.asyncTaskCallsBlock=100;
+		self.asyncTaskCallsBlock=0;  // do not use.... it breaks always
 		self.timeoutsCalled=0;
 		self.timeoutsAvoided=0;
 		self.lastTimeout=0;

52185e21ac35c84f8aec7c37dbce404ae2ba4fde
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-06-20 23:30:39 +0200

    using flatpickr

--- common/systemjs/ts/tab.structure.ts
+++ common/systemjs/ts/tab.structure.ts
@@ -33,6 +33,8 @@ export class TabStructure {
                     self.report.config.reuseIssues=bReuseIssues;
                 }
             });
+            flatpickr("#ReportInitDate", {});
+            flatpickr("#ReportEndDate", {});
         });
     }
  

5aba2c3225b16be742d492f9891f4addd1af1fca
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-06-27 06:38:46 +0200

    another try with steps

--- common/js/modelProcessor/jrfInteractive.js
+++ common/js/modelProcessor/jrfInteractive.js
@@ -1,18 +1,20 @@
 var jrfInteractive=class jrfInteractive {//this kind of definition allows to hot-reload
 	elemShowHide(elemId){
-		debugger;
-		var jqElem=$('#'+elemId);
-		var elem=jqElem[0];
-		if (isUndefined(elem.visible)
-			||
-			(isDefined(elem.visible)&&(!elem.visible))
-			){
-			jqElem.show();
-			elem.visible=true;
-		} else {
-			jqElem.hide();
-			elem.visible=false;
-		}
+        System.webapp.addStep("Show/Hide element", function(){
+        	debugger;
+    		var jqElem=$('#'+elemId);
+    		var elem=jqElem[0];
+    		if (isUndefined(elem.visible)
+    			||
+    			(isDefined(elem.visible)&&(!elem.visible))
+    			){
+    			jqElem.show();
+    			elem.visible=true;
+    		} else {
+    			jqElem.hide();
+    			elem.visible=false;
+    		}
+        },0,1,undefined,undefined,undefined,"GLOBAL_RUN");
 	}
 }
 

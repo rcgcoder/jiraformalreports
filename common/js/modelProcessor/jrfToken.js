85c6e42401a4ce2837ab00648a6d916b2f2f910e
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-02 06:37:41 +0200

    new try

--- common/js/modelProcessor/jrfToken.js
+++ common/js/modelProcessor/jrfToken.js
@@ -309,13 +309,13 @@ var jrfToken=class jrfToken{ //this kind of definition allows to hot-reload
 				self.indPostContentHtmlBuffer=self.pushHtmlBuffer();
 				self.addPostHtml();
 			}
-			var sContentDebug=self.topHtmlBuffer(self.indInnerContentHtmlBuffer).saToString();
+/*			var sContentDebug=self.topHtmlBuffer(self.indInnerContentHtmlBuffer).saToString();
 			if (sContentDebug.indexOf("table")>=0){
 				debugger;
 				loggerFactory.getLogger().enabled=true;
 				log(self.tag.getTagText()+" Post Processing:"+self.processVarsAtEnd);
 			}
-			if (self.processVarsAtEnd){
+*/			if (self.processVarsAtEnd){
 				var sContent=self.popHtmlBuffer(self.indInnerContentHtmlBuffer);
 				sContent=self.replaceVars(sContent);
 				self.addHtml(sContent);

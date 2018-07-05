2b2a62b90e6473bbdeb6c1ba083e4541dc059336
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-05 14:58:23 +0200

    async the loop processing

--- common/js/modelProcessor/jrfLoopBase.js
+++ common/js/modelProcessor/jrfLoopBase.js
@@ -17,18 +17,32 @@ var jrfLoopBase=class jrfLoopBase extends jrfSubset{//this kind of definition al
 		var self=this;
 		debugger;
 		self.loopElements=super.apply();
-		self.loopStart();
-		// processing total elements
-		self.loopElements.walk(function(loopElem){
-			if (self.innerVarName!=""){
-				self.initVariables(self.innerVarName,undefined,loopElem);
-			}
-			var bContinue=self.loopItemProcess(loopElem);
-			if (isDefined(bContinue)&&(!bContinue)){
-				return false;
-			}
+		self.addStep("Start processing the Loop",function(){
+			self.loopStart();
+			self.continueTask();
 		});
-		self.loopEnd();
+		// processing total elements
+		var iLoopCounter=0;
+		var bCancelLoop=false;
+		self.loopElements.walk(function(loopElem){
+			self.addStep("Processing the Loop item:"+iLoopCounter,function(){
+				if (!bCancelLoop) {
+					if (self.innerVarName!=""){
+						self.initVariables(self.innerVarName,undefined,loopElem);
+					}
+					var bContinue=self.loopItemProcess(loopElem);
+					if (isDefined(bContinue)&&(!bContinue)){
+						bCancelLoop=true;;
+					}
+				}
+				self.continueTask();
+			});
+			iLoopCounter++;
+		});
+		self.addStep("Ending processing the Loop",function(){
+			self.loopEnd();
+			self.continueTask();
+		});
 	}
 
 }

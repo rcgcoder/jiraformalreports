d96f3ac975cfa39f3299d7a72c0067d45170ca34
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-09 10:40:38 +0200

    forgotten!

--- common/js/modelProcessor/jrfNoop.js
+++ common/js/modelProcessor/jrfNoop.js
@@ -1,4 +1,4 @@
-var jrfActivateVar=class jrfActivateVar extends jrfToken{//this kind of definition allows to hot-reload
+var jrfNoop=class jrfNoop extends jrfToken{//this kind of definition allows to hot-reload
 	loadOwnProperties(){
 //		debugger;
 		var self=this;

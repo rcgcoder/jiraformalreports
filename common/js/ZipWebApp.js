5bfb0079e2aa532b2828179e2dfb3df4f9618559
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-10 13:20:20 +0200

    with proxy

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -42,7 +42,7 @@ class ZipWebApp{
 							"js/rcglibs/atlassian/RCGConfluence.js",
 							"js/rcglibs/atlassian/RCGJira.js",
 							"js/libs/he.js",
-							"html/empty.html"
+							"proxy:html/empty.html"
 /*							"js/rcglibs/RCGLogUtils.js",
 							"js/rcglibs/RCGChronoUtils.js",
 							"js/rcglibs/RCGHashMapUtils.js"

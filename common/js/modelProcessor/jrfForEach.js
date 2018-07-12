7d617c2f5b5da7484f4c4c7246b5c35bcc2ce7c8
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-12 06:48:34 +0200

    new try

--- common/js/modelProcessor/jrfForEach.js
+++ common/js/modelProcessor/jrfForEach.js
@@ -52,7 +52,7 @@ var jrfForEach=class jrfForEach extends jrfLoopBase{//this kind of definition al
 				iPosTR=self.model.htmlStack.saFindPos("<tr",true,iPosTR);
 			}
 			if (iPosTR>=0){
-				self.model.htmlStack.saReplace(iPosTR,3,'<tr id="trId_'+sAppendText+'_counter--'+self.counter+'_deep--'+iDeep+'_parentkey--'+sParentKey+'" ');
+				self.model.htmlStack.saReplace(iPosTR,3,'<tr id="trId_'+sAppendText+'_counter--'+self.counter+'_deep--'+iDeep+'_parentkey--'+sParentKey+'_visiParam--'+visiParam+'" ');
 			}
 			self.counter++;
 		}

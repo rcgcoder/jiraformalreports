5f7d9d2acb528d3566ca8e5edca70d9960dc8209
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-12 21:24:16 +0200

    change test

--- common/js/modelProcessor/jrfForEach.js
+++ common/js/modelProcessor/jrfForEach.js
@@ -90,7 +90,7 @@ var jrfForEach=class jrfForEach extends jrfLoopBase{//this kind of definition al
 			self.variables.pushVarEnv();
 			
 			if ((self.subType=="row")||(self.subType=="subrow")){
-				if (self.reportElem.getKey()=="NOTIFLOPD-124"){
+				if (isDefined(self.reportElem.getKey)&&(self.reportElem.getKey()=="NOTIFLOPD-124")){
 					debugger;
 				}
 				var treeParentId=self.variables.getVar("recursiveNodeId");

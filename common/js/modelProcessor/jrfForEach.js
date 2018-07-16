dbf10a4b3acd99a22464dbbe34ad2672e2efbce9
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-16 18:30:45 +0200

    remove unused debugs

--- common/js/modelProcessor/jrfForEach.js
+++ common/js/modelProcessor/jrfForEach.js
@@ -91,7 +91,7 @@ var jrfForEach=class jrfForEach extends jrfLoopBase{//this kind of definition al
 			
 			if ((self.subType=="row")||(self.subType=="subrow")){
 			/*	if (isDefined(self.reportElem.getKey)&&(self.reportElem.getKey()=="NOTIFLOPD-124")){
-					debugger;
+					//debugger;
 				} */
 				var treeParentId=self.variables.getVar("recursiveNodeId");
 				var treeNode=newHashMap();

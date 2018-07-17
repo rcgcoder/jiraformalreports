6b9107d64e8aee16609038922be86e651c19d04a
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-17 13:50:29 +0200

    new trace

--- common/js/modelProcessor/jrfSubset.js
+++ common/js/modelProcessor/jrfSubset.js
@@ -173,8 +173,8 @@ var jrfSubset=class jrfSubset extends jrfToken{//this kind of definition allows 
 	}
 	bounds(elemsInForEach){
 		var self=this;
-		var startAt=self.replaceVars(self.boundStartAt);
-		var limit=self.replaceVars(self.boundLimit);
+		var startAt=self.replaceVars(self.boundStartAt).replaceInnerHtmlTags().saToString().trim();
+		var limit=self.replaceVars(self.boundLimit).replaceInnerHtmlTags().saToString().trim();
 		if ((startAt=="")&&(limit=="")) return elemsInForEach;
 		var hsResult=newHashMap();
 		if (startAt==""){

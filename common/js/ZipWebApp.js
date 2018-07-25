45add8533685cdfef1459bf85806f071ca6aa2fa
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-25 19:19:31 +0200

    new try

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -42,9 +42,6 @@ class ZipWebApp{
 							"js/rcglibs/atlassian/RCGConfluence.js",
 							"js/rcglibs/atlassian/RCGJira.js",
 							"js/libs/he.js"
-//							"js/libs/xlsx/FileSaver.js",
-//							"js/libs/xlsx/shim.min.js",
-//							"js/libs/xlsx/xlsx.full.min.js"
 /*							"js/rcglibs/RCGLogUtils.js",
 							"js/rcglibs/RCGChronoUtils.js",
 							"js/rcglibs/RCGHashMapUtils.js"

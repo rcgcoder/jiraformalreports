ffb6f6be8063311c5c459d8c9f0eaaee87753d36
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-25 18:42:31 +0200

    new test with xlsx

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -41,7 +41,8 @@ class ZipWebApp{
 							"js/rcglibs/atlassian/RCGAtlassian.js",
 							"js/rcglibs/atlassian/RCGConfluence.js",
 							"js/rcglibs/atlassian/RCGJira.js",
-							"js/libs/he.js"
+							"js/libs/he.js",
+							"https://rawgit.com/SheetJS/js-xlsx/master/dist/xlsx.full.min.js"
 /*							"js/rcglibs/RCGLogUtils.js",
 							"js/rcglibs/RCGChronoUtils.js",
 							"js/rcglibs/RCGHashMapUtils.js"

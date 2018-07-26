454b1eaa000b83ce47e608682841227bc5fc46e8
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-07-26 20:16:51 +0200

    without debugger

--- common/js/modelProcessor/jrfSum.js
+++ common/js/modelProcessor/jrfSum.js
@@ -6,7 +6,6 @@ var jrfSum=class jrfSum extends jrfToken{//this kind of definition allows to hot
 	apply(){
 		var self=this;
 		//debugger;
-		debugger;
 		var varName=self.replaceVars(self.varName).saToString().trim();
 		var hsValues=self.variables.getVars(varName);
 		var sValue=0;

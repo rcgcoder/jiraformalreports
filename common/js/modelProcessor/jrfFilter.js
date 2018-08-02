60a2113f84b9a7174be01120480a7ff3e53b51cc
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-08-02 06:42:09 +0200

    new change

--- common/js/modelProcessor/jrfFilter.js
+++ common/js/modelProcessor/jrfFilter.js
@@ -8,6 +8,7 @@ var jrfFilter=class jrfFilter extends jrfNoop{//this kind of definition allows t
 		var self=this;
 		super.endApplyToken();
 		var sFilterBody=self.popHtmlBuffer(self.indInnerContentHtmlBuffer);
+		sFilterBody="("+sFilterBody.saToString().trim()+")";
 //		sFilterBody=sFilterBody.saRemoveInnerHtmlTags(""); // NOT NEEDED THE CLEAN FUNCTION REMOVES
 		var fltName=self.replaceVars(self.filterName);
 		self.model.filters.newFilter(fltName.saToString().trim(),sFilterBody);

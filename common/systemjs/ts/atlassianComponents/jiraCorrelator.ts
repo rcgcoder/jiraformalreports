544223900a7a73143878cf820e2053edde1d438f
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-08-16 10:25:13 +0200

    with label

--- common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
+++ common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
@@ -15,8 +15,6 @@ export class jiraCorrelator {
             log("PostProcessing:"+self.name);
             System.bindObj(self);
             self.changeVisibilityAndOr();
-            var tglEnabler=System.getAngularObject(self.name+"-toggle",true);
-            tglEnabler.setLabel(self.toggleLabel);
             if (!self.withCaption){
                 var auxObj=System.getAngularDomObject(self.name+"-caption");
                 $(auxObj).hide();

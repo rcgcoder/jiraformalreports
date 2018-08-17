c353197b20380ad75820088247bf47af6198746b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-08-17 08:22:35 +0200

    new try.... with height

--- common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
+++ common/systemjs/ts/atlassianComponents/jiraCorrelator.ts
@@ -175,8 +175,11 @@ export class jiraCorrelator {
         }
 //        this.getTextArea().val(this.theScript);
         if (isUndefined(this.editor)){
+            var auxObj=System.getAngularDomObject(self.name+"-DialogBody");
+            var bodyHeight=$(auxObj).height();
             ace.config.set('basePath', System.webapp.composeUrl("js/libs/ace/src-noconflict")); 
             this.editor = ace.edit(self.name+"-ace");
+            this.editor.height(bodyHeight+ "px");
             this.editor.setTheme("ace/theme/monokai");
             this.editor.session.setMode("ace/mode/javascript");
             this.editor.setValue(this.theScript);

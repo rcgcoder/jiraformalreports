016b451e2a40bafdff2d3e3e11ff94f15e638933
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-09-04 14:34:35 +0200

    call deep = 25

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -98,7 +98,7 @@ var jrfReport=class jrfReport {
 
 	execute(bDontReloadFiles){
 		var self=this;
-		var auxAsyncCallsMaxDeep=30;
+		var auxAsyncCallsMaxDeep=25;
 		var tm=self.getTaskManager();
 		tm.asyncTimeWasted=0;
 		tm.asyncTaskCallsBlock=0;

3467ba5ea810d39c792eb421354df81394a46e41
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-09-05 08:28:35 +0200

    all timeouts

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -99,7 +99,7 @@ var jrfReport=class jrfReport {
 
 	execute(bDontReloadFiles){
 		var self=this;
-		var auxAsyncCallsMaxDeep=10;
+		var auxAsyncCallsMaxDeep=0;
 		var dontReturnAllIssuesRetrieved=true;
 		var tm=self.getTaskManager();
 		tm.getStackTraceLinesTime=0;

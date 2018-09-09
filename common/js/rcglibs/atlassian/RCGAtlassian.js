15d9a37c4434a3bc7d3b95e9b58ac2a458b1a25f
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-09-09 07:31:50 +0200

    new try

--- common/js/rcglibs/atlassian/RCGAtlassian.js
+++ common/js/rcglibs/atlassian/RCGAtlassian.js
@@ -164,7 +164,7 @@ class RCGAtlassian{
 						if (isDefined(callback)){
 							self.addStep("Calling the user callback",function(response,xhr,sUrl,headers){
 								callback(response,xhr,sUrl,headers);
-								self.continuetTask([response,xhr,sUrl,headers]);
+								self.continueTask([response,xhr,sUrl,headers]);
 							});
 						}
 						self.continueTask();

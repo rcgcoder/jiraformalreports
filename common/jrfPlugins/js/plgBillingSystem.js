30c982a17e55ba6472f311a398971d3e3d9fe354
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-09-11 00:16:36 +0200

    new try

--- common/jrfPlugins/js/plgBillingSystem.js
+++ common/jrfPlugins/js/plgBillingSystem.js
@@ -816,7 +816,7 @@ var plgBillingSystem=class plgBillingSystem{//this kind of definition allows to 
 	   	});
 	   	if (isDefined(baseSnapshot)){
 	   		var fncAdjustNoFacturable=function(auxSnapshot){
-	   			debugger;
+	   			//debugger;
 		   		var auxImporteNoFacturable=auxSnapshot.calculos.aprobado+auxSnapshot.calculos.pendiente;
 		   		var auxTsNoFacturable=auxSnapshot.calculos.inTimespents.aprobado+auxSnapshot.calculos.inTimespents.pendiente;
 		   		var avanceImporte=0;

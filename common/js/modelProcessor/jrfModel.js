f39dd94c43a611586901e7b9aa22d85bf2a8d9b6
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-09-12 18:13:09 +0200

    new try

--- common/js/modelProcessor/jrfModel.js
+++ common/js/modelProcessor/jrfModel.js
@@ -277,9 +277,9 @@ var jrfModel=class jrfModel{ //this kind of definition allows to hot-reload
 			auxTag.setPreviousHTML(previousHtml);
 
 			sTagText=arrJRFs[auxIndex];
-			if (sTagText.indexOf("apt=7")>=0){
+			/*if (sTagText.indexOf("apt=7")>=0){
 				debugger;
-			}
+			}*/
 			log("JRF tag index:"+auxIndex);
 			log("Row text:"+sTagText);
 			sNewPostText="";

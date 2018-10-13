7ee23b316fba08209dfdb86bc644786131ed4a75
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-13 07:51:52 +0200

    big test...

--- common/js/rcglibs/tests/RCGTest.js
+++ common/js/rcglibs/tests/RCGTest.js
@@ -20,7 +20,7 @@ System.webapp.addStep("hashMap",function(){
 		var hsAux=newHashMap();
 		hsAux.autoSwing=false;
 		if (iDeepMax<0) return hsAux;
-		for (var i=0;i<30;i++){
+		for (var i=0;i<35;i++){
 			var vRnd=(Math.random()*100);
 			var vKey=Math.round(vRnd/10);
 			if (vRnd<10){

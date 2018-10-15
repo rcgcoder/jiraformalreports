43bae369fa25f0663a7ec55292837838ab3e57ff
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-15 07:26:34 +0200

    without proto

--- common/js/jrfIssueFactory.js
+++ common/js/jrfIssueFactory.js
@@ -34,7 +34,9 @@ function newIssueFactory(report){
 			,
 			[]
 			,
-			"Issue");
+			undefined
+			//"Issue"
+			);
 	dynObj.hsExcludedProjects=newHashMap();	
 	dynObj.addExcludedProject=function(prjKey){
 		var self=this;

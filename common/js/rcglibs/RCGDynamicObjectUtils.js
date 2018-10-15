b4cad2112b3e063592a2bfe753d1f37106576132
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-15 12:34:19 +0200

    new test

--- common/js/rcglibs/RCGDynamicObjectUtils.js
+++ common/js/rcglibs/RCGDynamicObjectUtils.js
@@ -955,7 +955,7 @@ var factoryObjects=class factoryObjects{
 	getStorageObject(storer){
 		var self=this; //self is an individual object
 		var objResult={};
-		objResult.key=self.getID();
+		objResult.key=self.id;
 		self.factory.attrTypes.walk(function(value,deep,key){
 			var attrName=key;
 			var attrType=value.type;

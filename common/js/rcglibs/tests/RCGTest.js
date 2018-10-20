9f84c4c970b7aee08fa0b83de79248c76fecbd1d
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-21 00:44:00 +0200

    it works test with 1500 childs

--- common/js/rcglibs/tests/RCGTest.js
+++ common/js/rcglibs/tests/RCGTest.js
@@ -131,7 +131,7 @@ System.webapp.addStep("Dynamic Object With Childs",function(){
 	auxObj.setTestOneString("Tested String Values");
 	auxObj.addTestStringList("One Value for String List");
 	auxObj.addTestStringList("Second Value for String List");
-	var nTotalChilds=15;
+	var nTotalChilds=1500;
 	debugger;
 	storer.addStep("Filling a lot ( "+nTotalChilds+" ) of childs",function(){
 		var nChilds=0;

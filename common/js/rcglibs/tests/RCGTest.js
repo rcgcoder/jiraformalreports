0c72c430dd8727a8c84debea306420d6da26632a
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-20 08:19:55 +0200

    with load at end

--- common/js/rcglibs/tests/RCGTest.js
+++ common/js/rcglibs/tests/RCGTest.js
@@ -158,7 +158,6 @@ System.webapp.addStep("Dynamic Object With Childs",function(){
 		});
 		storer.continueTask();
 	});
-	/*
 	storer.addStep("Parallelize the full load test",function(result){
 		//walkAsync(sName,callNode,callEnd,callBlockPercent,callBlockTime,secsLoop,hsOtherParams,barrier){
 		var fncLoaded=function(oneChild){
@@ -170,7 +169,7 @@ System.webapp.addStep("Dynamic Object With Childs",function(){
 			storer.continueTask();
 		};
 		storer.parallelizeCalls(result.getChilds(),fncLoad,fncLoaded,5);
-	});*/
+	});
 	storer.continueTask();
 });
 

4b7de073a08485717eb14b5c5ece56134a9eff87
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-26 16:21:57 +0200

    changes in hashmap

--- common/js/rcglibs/RCGHashMapUtils.js
+++ common/js/rcglibs/RCGHashMapUtils.js
@@ -1474,26 +1474,30 @@ class RCGHashMapFactory{
 								prevAux.last="";
 							//}
 							chronoStart("Asignamos_izda",prevAux.key);
-//							if (prevAux.left==""){
+							if ((prevAux.left=="")||(prevAux.left.key==pos.key)){
 								prevAux.left=pos.left; // colgamos todos los izquierdos del antiguo en el izquierdo del sustituto
 								if (prevAux.left!=""){ // si el nuevo izquierdo es node
 		//							chronoStart("node Asignado",prevAux.left.key);
 									prevAux.left.parent=prevAux; // asignamos el sustituto como parent
 		//							chronoStop();
 								}
-	//						} else {
-	//							alert("Error al eliminar node.. el Menor de los Mayores no debería tener IZQUIERDO:"+key);
-	//							vUndef.peta("MegaError");
-	//						}
+							} else {
+								alert("Error al eliminar node.. el Menor de los Mayores no debería tener IZQUIERDO:"+pos.key);
+								vUndef.peta("MegaError");
+							}
 							chronoStop();
 							chronoStart("Asignamos_dcha",prevAux.key);
 							// si el node a sustitutir tenia nodes a la right hay que ponerlos a la right del sustituto
-							// if (pos.right!=""){ // el node a sustituir en esta rama siempre tiene nodes a la right
+							//if (pos.right!=""){ // el node a sustituir en esta rama siempre tiene nodes a la right
+							if ((prevAux.right=="")||(prevAux.right.key==pos.key)){
 								prevAux.right=pos.right;
 								if (prevAux.right!=""){
 									prevAux.right.parent=prevAux;
 								}
-							//}
+							} else {
+								alert("Error al eliminar node.. no debería tener Derecho:"+pos.key);
+								vUndef.peta("MegaError");
+							}
 							chronoStop();
 							this.updateChildNumber(theParent,1+prevAux.brothers.length);
 							chronoStop();

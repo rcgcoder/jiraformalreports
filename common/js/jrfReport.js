67c66f1095f7773b783da717119a8b68505dedb3
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-10-29 11:39:07 +0100

    find cycles

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -1241,7 +1241,7 @@ var jrfReport=class jrfReport {
 						self.continueTask();
 					});
 					self.addStep("Report is Checking the issue "+issue.getKey(),function(){
-//						issue.checkChildCycles(self);
+						issue.checkChildCycles(self);
 						self.continueTask();
 					});
 				},1);

461c824ef60d1879e28a68efd54244c4b8e29d08
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-11-05 00:08:49 +0100

    new try

--- common/js/rcglibs/RCGBaseUtils.js
+++ common/js/rcglibs/RCGBaseUtils.js
@@ -5,10 +5,48 @@ if (typeof require!=="undefined"){
 	var shell = require('shelljs' );
 	var StackUtils=require("./StackUtils.js");
 }
-
 function isInNodeJS(){
 	return bInNodeJS;
 }
+
+//Only add setZeroTimeout to the window object, and hide everything
+//else in a closure.
+(function() {
+ var timeouts = [];
+ var messageName = "zero-timeout-message";
+
+ // Like setTimeout, but only takes a function argument.  There's
+ // no time argument (always zero) and no arguments (you have to
+ // use a closure).
+ function setZeroTimeout(fn) {
+     timeouts.push(fn);
+     window.postMessage(messageName, "*");
+ }
+
+ function handleMessage(event) {
+     if ((event.source == window) && (event.data == messageName)) {
+         event.stopPropagation();
+         if (timeouts.length > 0) {
+             var fn = timeouts.shift();
+             fn();
+         }
+     }
+ }
+
+ window.addEventListener("message", handleMessage, true);
+
+ // Add the one thing we want added to the window object.
+ window.setZeroTimeout = setZeroTimeout;
+})();
+
+
+function getMemStatus(){
+	var giga=(1024*1024);
+	return " totalJSHeapSize:"+ (performance.memory.totalJSHeapSize/giga).toFixed(3) + ',' +
+			"usedJSHeapSize:"  + (performance.memory.usedJSHeapSize/giga).toFixed(3)  + ',' +
+			"jsHeapSizeLimit:" + (performance.memory.jsHeapSizeLimit/giga).toFixed(3);
+}
+
 function clone(srcObj){
 	var result={};
 	var arrProperties=Object.getOwnPropertyNames(srcObj);

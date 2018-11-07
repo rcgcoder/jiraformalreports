e983b153544e572e9a7a84605c6870861c49fc7b
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-11-07 14:58:27 +0100

    only 100

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -526,7 +526,7 @@ var jrfReport=class jrfReport {
 		// first launch all issue retrieve ...
 		self.addStep("Getting All Issues in the Scope.... ",function(){
 			debugger;
-			self.allIssues.changeStorableParams(1000,0.10,true);
+			self.allIssues.changeStorableParams(100,0.10,true);
 			if (self.isReusingIssueList()){
 				self.addStep("check if report exists in storage",function(){
 					return self.existStored(); // load all issues

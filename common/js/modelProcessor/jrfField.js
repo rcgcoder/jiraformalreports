e64ec9873e89ccad3ac6591fcca98d25ca1b81b1
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-11-10 07:51:43 +0100

    jrffield can include exception and retry

--- common/js/modelProcessor/jrfField.js
+++ common/js/modelProcessor/jrfField.js
@@ -15,23 +15,25 @@ var jrfField=class jrfField extends jrfToken{//this kind of definition allows to
 			log("There is not function.... in reportElem:it appears to be a jrfReport element");
 		} else {
 			log("There is function fieldValue.... in reportElem:"+self.reportElem.getKey());
-			sValue=self.reportElem.fieldValue(self.fieldName,true,self.datetime,self.otherParams);
-			if (isString(sValue)&&(sValue.indexOf("&lt;jrf")>=0)){// if there is jrf tokens in the description
-				var sHtml=decodeEntities(sValue);
-				var theModel;
-				self.addStep("Processing the field including the jrf tags",function(){
-					theModel=new jrfModel(self.model.report,sHtml,self.reportElem);
-					theModel.variables=self.model.variables;
-					theModel.variables.pushVarEnv();
-					theModel.process(); 
-				});
-				self.addStep("Setting the HTML",function(sModelProcessedResult){
-					self.addHtml(sModelProcessedResult);
-					theModel.variables.popVarEnv();
-				});
-			} else {
-				self.addHtml(sValue);
-			}
+			self.executeAsStepMayRetry(false,function(){
+				sValue=self.reportElem.fieldValue(self.fieldName,true,self.datetime,self.otherParams);
+				if (isString(sValue)&&(sValue.indexOf("&lt;jrf")>=0)){// if there is jrf tokens in the description
+					var sHtml=decodeEntities(sValue);
+					var theModel;
+					self.addStep("Processing the field including the jrf tags",function(){
+						theModel=new jrfModel(self.model.report,sHtml,self.reportElem);
+						theModel.variables=self.model.variables;
+						theModel.variables.pushVarEnv();
+						theModel.process(); 
+					});
+					self.addStep("Setting the HTML",function(sModelProcessedResult){
+						self.addHtml(sModelProcessedResult);
+						theModel.variables.popVarEnv();
+					});
+				} else {
+					self.addHtml(sValue);
+				}
+			});
 		}
 	}
 

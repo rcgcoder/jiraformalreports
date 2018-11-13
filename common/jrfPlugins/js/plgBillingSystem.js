54c57f6cde38530abd83c63e31e8560130a93440
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-11-13 06:37:39 +0100

    new try

--- common/jrfPlugins/js/plgBillingSystem.js
+++ common/jrfPlugins/js/plgBillingSystem.js
@@ -338,7 +338,7 @@ var plgBillingSystem=class plgBillingSystem{//this kind of definition allows to 
 		})
 
 		report.addStep("Accumulating timestimate value of childs",function(){
-			alert("Accumulating timestimate");
+//			alert("Accumulating timestimate");
 			debugger;
 			return self.fieldAccumChilds("timeestimate",atDatetime);
 		});

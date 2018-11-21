3f0d325bf6fe81ec44ef57b1b2af887250f27b22
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-11-21 21:49:02 +0100

    new try

--- common/js/jrfReport.js
+++ common/js/jrfReport.js
@@ -385,6 +385,11 @@ var jrfReport=class jrfReport {
 			});
 			self.addStep("Process XLSX",function(sRelativePath,content,contentType,theWindow){
 				debugger;
+				
+				  var data = new Uint8Array(content);
+				  var arr = new Array();
+				  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+				  var bstr = arr.join("");
 				  var workbook = XLSX.read(content, {
 			        type: 'binary'
 			      });

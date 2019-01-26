a0d880e9ff63ef1bd599c4abcb76b654b8fc1e1c
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-26 08:57:59 +0100

    new try

--- common/js/ZipWebApp.js
+++ common/js/ZipWebApp.js
@@ -162,6 +162,7 @@ class ZipWebApp{
 					  "RSA-SHA1",
 					  null,
 					  "privateKeyData");
+			consumer.get("url", "oauth_token", "oauth_token_secret", "content_type",  "callback");
 			var atl=self.getAtlassian();
 			atl.initialized=true;
 		});  

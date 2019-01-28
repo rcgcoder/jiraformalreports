440c6faad1c0dca71831308002664866d89c7030
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-28 13:16:19 +0100

    new try

--- common/systemjs/config.js
+++ common/systemjs/config.js
@@ -35,6 +35,7 @@ System.config({
    // 'isBuffer':'npm:isbuffer@0.0.0',
 //    'util': 'npm:util@0.11.1' ,
    // 'punycode':'npm:punycode@1.4.1',
+   // 'url':'js/libs/node-libs/url',
     'url':'npm:url@0.11.0',
 //    'crypto':'npm:crypto@1.0.1',
 //    'oauth-js':'npm:oauth-js@0.9.9',
@@ -59,11 +60,11 @@ System.config({
     	main: './index.js',
     	defaultExtension: 'js'
     },
-    util: {
+/*    util: {
     	main: './util.js',
     	defaultExtension: 'js' 
     },
-    url: {
+*/    url: {
     	main: './url.js',
     	defaultExtension: 'js'
     }

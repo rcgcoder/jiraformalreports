cf61575de5e8bf7c466747b697fd2ff07848a8c2
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-28 10:08:45 +0100

    using jsdlivr oauth-js

--- common/systemjs/config.js
+++ common/systemjs/config.js
@@ -37,8 +37,7 @@ System.config({
    // 'punycode':'npm:punycode@1.4.1',
    // 'url':'npm:url@0.11.0',
 //    'crypto':'npm:crypto@1.0.1',
-    'oauth':'js/libs/oauth/oauth.js',
-    'sha1':'js/libs/oauth/sha1.js',
+    'oauth':'npm:oauth-js@0.9.9',
 //    'querystring':'npm:querystring@0.2.0',
     'typescript': 'npm:typescript@2.0.2/lib/typescript.js'
     

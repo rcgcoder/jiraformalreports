f2350fff127e06f9182bd61370f9b51dbd7fc25e
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-28 13:17:13 +0100

    new try

--- common/systemjs/config.js
+++ common/systemjs/config.js
@@ -36,7 +36,7 @@ System.config({
 //    'util': 'npm:util@0.11.1' ,
    // 'punycode':'npm:punycode@1.4.1',
    // 'url':'js/libs/node-libs/url',
-    'url':'npm:url@0.11.0',
+    'url':'js/libs/node-libs/url',
 //    'crypto':'npm:crypto@1.0.1',
 //    'oauth-js':'npm:oauth-js@0.9.9',
     'oauth':'npm:oauth-js@0.9.9',

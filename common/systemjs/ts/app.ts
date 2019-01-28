78109d34574e1e184d3a05e55b54a8212a1f0a6c
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-28 12:49:49 +0100

    new try

--- common/systemjs/ts/app.ts
+++ common/systemjs/ts/app.ts
@@ -9,7 +9,7 @@ import {isBuffer} from 'isBuffer';
 */
 //import {crypto} from 'crypto';
 //import {sha1} from 'sha1';
-//import {OAuth} from 'oauth';
+import {OAuth} from 'oauth';
 import {Tabs} from './tabs';
 import {Tab} from './tab';
 import {TabReports} from './tab.reports';
@@ -118,7 +118,7 @@ class App {
   schemas: [
             CUSTOM_ELEMENTS_SCHEMA
         ],
-  imports: [ BrowserModule,util,OAuth ],
+  imports: [ BrowserModule,OAuth ],
   declarations: [   App, 
                     Tabs, 
                     Tab, 

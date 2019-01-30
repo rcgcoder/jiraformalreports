b3f5aef1b3dd73f38ad757ecd63c930c7cf83dd8
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-30 05:53:56 +0100

    restore working version

--- common/systemjs/config.js
+++ common/systemjs/config.js
@@ -32,7 +32,9 @@ System.config({
     '@angular/router/testing': 'npm:@angular/router@5.2.10/bundles/router-testing.umd.js',
     'rxjs': 'npm:rxjs@5.5',
     'odt': 'npm:odt@1.1.0',
-    'typescript': 'npm:typescript@2.8.3/lib/typescript.js'
+    'typescript': 'npm:typescript@2.0.2/lib/typescript.js'
+    
+//    'typescript': 'npm:typescript@2.8.3/lib/typescript.js'
   },
   //packages defines our app package
   packages: {
@@ -42,6 +44,6 @@ System.config({
     },
     rxjs: {
       defaultExtension: 'js'
-    }    
+    }
   }
 });
--- common/systemjs/ts/app.ts
+++ common/systemjs/ts/app.ts
@@ -1,15 +1,7 @@
 //our root app component
 import {Component, NgModule, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-//import {util} from 'util';
-//import {url} from 'url';
-/*//import {querystring} from 'querystring';
-/*import {punycode} from 'punycode';
-import {isBuffer} from 'isBuffer';
-*/
-//import {crypto} from 'crypto';
-//import {sha1} from 'sha1';
-//import {OAuth} from 'oauth';
+
 import {Tabs} from './tabs';
 import {Tab} from './tab';
 import {TabReports} from './tab.reports';
@@ -51,7 +43,7 @@ class App {
         self.addStep("Systemjs components Initializacion",function(){
             log("Running global systemjs components initialization");
             self.addStep("Applying AUI and other interface components.... ",function(){
-            	var arrFiles=[
+                var arrFiles=[
                               "js/libs/flatpickr/flatpickr.css",
                               "js/libs/flatpickr/flatpickr.js",
                               "aui/js/aui.js",
@@ -60,12 +52,10 @@ class App {
                               "aui/js/aui-experimental.js",
                               "aui/js/aui-datepicker.js",
                               "aui/js/aui-soy.js"
-                            //  "js/libs/oauth/oauth.js"
                            ]; //test
                 return System.webapp.loadRemoteFiles(arrFiles);
             });
             self.addStep("Postprocessing systemjs components.... ",function(){
-    			log("Initialize ends.. ");
                 return self.parallelizeProcess(System.postProcess.length,function(iPostFunction){
                     //log("PostProcess "+iPostFunction);
                     return System.postProcess[iPostFunction]();
@@ -101,7 +91,7 @@ class App {
   schemas: [
             CUSTOM_ELEMENTS_SCHEMA
         ],
-  imports: [ BrowserModule,Oauth],
+  imports: [ BrowserModule ],
   declarations: [   App, 
                     Tabs, 
                     Tab, 

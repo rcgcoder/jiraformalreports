b6cd3dda5879a039578930eea253ae0da71c2afb
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-31 06:40:17 +0100

    a simple test

--- common/js/libs/oauth/test.js
+++ common/js/libs/oauth/test.js
@@ -1,5 +1,21 @@
 
 
 debugger;
-var theOauth=window.oauthSigner();
-document.write("Hello"+theOauth.version);
+var theMethod="GET";
+var theUrl="https://paega2.atlassian.net/secure/attachment/41486/screenshot-1.png";
+var theParameters={
+    oauth_consumer_key: "OauthKeyT"
+	,oauth_nonce: "8302830"
+	,oauth_signature_method: "HMAC-SHA1"
+	,oauth_timestamp: 1548825765
+	,oauth_token: "asfFgx4OZZAqAQsLBL4bTnjOq91P1NDE"
+	,oauth_version: "1.0"
+};
+var theConsumerSecret="     ";
+var theTokenSecret="   ";
+
+var signatureNew = oauthSignature.generate(theMethod, theUrl, theParameters,
+        theConsumerSecret, theTokenSecret);
+var theBaseString=new oauthSignature.SignatureBaseString(theMethod, theUrl, theParameters).generate();
+
+document.write("Hello:  "+signatureNew );

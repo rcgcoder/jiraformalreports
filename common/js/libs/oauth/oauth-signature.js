0fa60a7f33450b2f6ac716e827fe1f29ad445890
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-01-31 20:43:10 +0100

    new try

--- common/js/libs/oauth/oauth-signature.js
+++ common/js/libs/oauth/oauth-signature.js
@@ -45,10 +45,13 @@ e;d++)if(d%4){var g=f.indexOf(b.charAt(d-1))<<2*(d%4),h=f.indexOf(b.charAt(d))>>
 			return new HmacSha1Signature(signatureBaseString, consumerSecret, tokenSecret).generate(encodeSignature);
 		} else {
 			console.log("SBS:" + signatureBaseString + " CS:" +consumerSecret + " TS: "+tokenSecret+" ES:"+encodeSignature);
-			var theKey = /*oSignatureBaseString._rfc3986.encode(consumerSecret) + '&' + */ oSignatureBaseString._rfc3986.encode(tokenSecret);
+			var theKey = /*oSignatureBaseString._rfc3986.encode(consumerSecret) + '&' + */ 
+						  oSignatureBaseString._rfc3986.encode(tokenSecret);
 			var sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA"});
 			// initialize for signature generation
-			var rsaKey=toPEM(theKey);
+			var rsaKey=`-----BEGIN RSA PRIVATE KEY-----
+			`+theKey+` 
+			-----END RSA PRIVATE KEY-----`;
 			sig.init(rsaKey); // rsaPrivateKey of RSAKey object
 			// update data
 			sig.updateString(signatureBaseString);

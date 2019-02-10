09db38a4af056540bc341fa7e1881e994bc44c1f
rcantabrana <rcantabrana@A0108030.ARAGON>
2019-02-10 06:38:04 +0100

    new try

--- common/js/rcglibs/RCGTaskManager.js
+++ common/js/rcglibs/RCGTaskManager.js
@@ -1213,7 +1213,7 @@ class RCGTaskManager{
 			arrStatus.push(tm.globalForks[i].getStatus());
 		}
 		var endTime=new Date().getTime();
-		log("getStatus wasted time:"+((endTime-initTime)/1000));
+		logError("getStatus wasted time:"+((endTime-initTime)));
 		return arrStatus;
 	}
 	extended_createManagedFunction(fncTraditionalFunction){
